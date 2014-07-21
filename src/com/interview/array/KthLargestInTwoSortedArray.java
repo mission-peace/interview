28c7e7cf66f0b1afff721220302513cf669944c1
mission-peace <tusroy@0c4de9a70437.ant.amazon.com>
2014-07-20 19:50:13 -0700

    Merging two classes of kth largest in one class

--- src/com/interview/array/KthLargestInTwoSortedArray.java
+++ src/com/interview/array/KthLargestInTwoSortedArray.java
@@ -5,6 +5,42 @@ package com.interview.array;
  */
 public class KthLargestInTwoSortedArray {
 
+	
+	public int kthLargest1(int arr1[],int arr2[],int low1,int high1,int low2,int high2,int k){
+		int len1 = high1-low1 +1;
+		int len2 = high2 - low2+1;
+		
+		if(len1 == 0){
+			return arr2[low2+k];
+		}
+		if(len2 ==0){
+			return arr1[low1+k];
+		}
+		if(k == 0){
+			return arr1[low1] < arr2[low2] ? arr1[low1] : arr2[low2];
+		}
+		
+		//TODO - understand how mid is calculated
+		int mid1 = len1*k/(len1 + len2);
+		int mid2 = k - mid1 - 1;
+		
+		mid1 = low1+mid1;
+		mid2 = low2 + mid2;
+		
+		if(arr1[mid1] > arr2[mid2]){
+			k = k - mid2 + low2 -1;
+			high1 = mid1;
+			low2 = mid2 + 1;
+		}else{
+			k = k - mid1 + low1 -1;
+			high2 = mid2;
+			low1 = mid1+1;
+		}
+		return kthLargest(arr1, arr2, low1, high1, low2, high2, k);
+	}
+
+	
+	
 	public int kthLargest(int input1[],int input2[],int k){
 		return kthLargest(input1, input2, 0, input1.length-1, 0, input2.length-1, k);
 	}
