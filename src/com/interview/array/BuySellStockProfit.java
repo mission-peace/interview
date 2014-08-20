9bf6c5ace6b5641f865c2eefd45436fb52c59bfd
mission-peace <tusroy@0c4de9a70437.ant.amazon.com>
2014-08-19 20:55:02 -0700

    Fix bug

--- src/com/interview/array/BuySellStockProfit.java
+++ src/com/interview/array/BuySellStockProfit.java
@@ -2,40 +2,42 @@ package com.interview.array;
 
 public class BuySellStockProfit {
 
-	public int oneProfit(int arr[]){
-		int minPrice = arr[0];
-		int maxProfit = 0;
-		for(int i=1; i < arr.length; i++){
-			if(arr[i] - minPrice > maxProfit){
-				maxProfit = arr[i] - minPrice;
-			}
-			if(arr[i] < minPrice){
-				minPrice = arr[i];
-			}
-		}
-		return maxProfit;
-	}
-	
-	public int allTimeProfit(int arr[]){
-		int profit = 0;
-		int localMin = arr[0];
-		for(int i=1; i < arr.length;i++){
-			if(arr[i-1] < arr[i]){
-				continue;
-			}
-			
-			profit += arr[i-1] - localMin;
-			localMin = arr[i];
-		}
-		return profit;
-	}
-	
-	public static void main(String args[]){
-		int arr[] = {7,10,15,5,11,2,7,9,3};
-		BuySellStockProfit bss = new BuySellStockProfit();
-		System.out.println(bss.oneProfit(arr));
-		
-		System.out.print(bss.allTimeProfit(arr));
-		
-	}
+    public int oneProfit(int arr[]){
+        int minPrice = arr[0];
+        int maxProfit = 0;
+        for(int i=1; i < arr.length; i++){
+            if(arr[i] - minPrice > maxProfit){
+                maxProfit = arr[i] - minPrice;
+            }
+            if(arr[i] < minPrice){
+                minPrice = arr[i];
+            }
+        }
+        return maxProfit;
+    }
+    
+    public int allTimeProfit(int arr[]){
+        int profit = 0;
+        int localMin = arr[0];
+        for(int i=1; i < arr.length;i++){
+            if(arr[i-1] >= arr[i]){
+                localMin = arr[i];
+            }else{
+                profit += arr[i] - localMin;
+                localMin = arr[i];
+            }
+            
+        }
+        return profit;
+    }
+    
+    public static void main(String args[]){
+        int arr[] = {7,10,15,5,11,2,7,9,3};
+        int arr1[] = {6,4,1,3,5,7,3,1,3,4};
+        BuySellStockProfit bss = new BuySellStockProfit();
+        System.out.println(bss.oneProfit(arr));
+        
+        System.out.print(bss.allTimeProfit(arr1));
+        
+    }
 }
