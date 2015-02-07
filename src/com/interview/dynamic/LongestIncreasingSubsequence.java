120b49c37e79f9b6e09cc785f1387f3ee3654799
Roy <tusroy@amazon.com>
2015-02-06 19:00:55 -0800

    Printing actual solution in lis question

--- src/com/interview/dynamic/LongestIncreasingSubsequence.java
+++ src/com/interview/dynamic/LongestIncreasingSubsequence.java
@@ -5,28 +5,44 @@ package com.interview.dynamic;
  */
 public class LongestIncreasingSubsequence {
 
-    public int longestSubsequence(int arr[]){
+    public int longestSubsequenceWithActualSolution(int arr[]){
         
         int temp[] = new int[arr.length];
-        
+        int actualSolution[] = new int[arr.length];
         for(int i=0; i < arr.length; i++){
             temp[i] = 1;
+            actualSolution[i] = i;
         }
         
         for(int i=1; i < arr.length; i++){
             for(int j=0; j < i; j++){
                 if(arr[i] > arr[j]){
-                    temp[i] = temp[j] + 1> temp[i] ? temp[j] + 1 : temp[i];
+                    if(temp[j] + 1 > temp[i]){
+                        temp[i] = temp[j] + 1;
+                        //set the actualSolution to point to guy before me
+                        actualSolution[i] = j;
+                    }
                 }
             }
         }
-        int max = 0;
+        int maxIndex = 0;
         for(int i=0; i < temp.length; i++){
-            if(temp[i] > max){
-                max = temp[i];
+            if(temp[i] > temp[maxIndex]){
+                maxIndex = i;
             }
         }
-        return max;
+        
+        //lets print the actual solution
+        int t = maxIndex;
+        int newT = maxIndex;
+        do{
+            t = newT;
+            System.out.print(arr[t] + " ");
+            newT = actualSolution[t];
+        }while(t != newT);
+        System.out.println();
+ 
+        return temp[maxIndex];
     }
     
     public int longestSubsequenceRecursive(int arr[]){
@@ -66,7 +82,7 @@ public class LongestIncreasingSubsequence {
         LongestIncreasingSubsequence lis = new LongestIncreasingSubsequence();
         int arr[] = {23,10,22,5,33,8,9,21,50,41,60,80};
     //  int arr[] = {7,4,7,8};
-        int result = lis.longestSubsequence(arr);
+        int result = lis.longestSubsequenceWithActualSolution(arr);
         int result1 = lis.longestSubsequenceRecursive(arr);
         System.out.println(result);
         System.out.println(result1);
