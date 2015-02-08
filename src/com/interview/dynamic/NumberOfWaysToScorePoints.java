9c9a5ad0c2fae1aac7038e8f121b1669e874a432
Roy <tusroy@amazon.com>
2015-02-07 17:08:19 -0800

    Fixing tab space

--- src/com/interview/dynamic/NumberOfWaysToScorePoints.java
+++ src/com/interview/dynamic/NumberOfWaysToScorePoints.java
@@ -8,40 +8,40 @@ package com.interview.dynamic;
  */
 public class NumberOfWaysToScorePoints {
 
-	//in this version to make 3 - 1,2 is same as 2,1.
-	//This is similar to coin changing problem
-	public int version1(int score[],int total){
-		int T[] = new int[total+1];
-		T[0] = 1;
-		for(int i=0; i < score.length; i++){
-			for(int j=1; j <= total; j++){
-				if(score[i] <= j){
-					T[j] += T[j-score[i]];
-				}
-			}
-		}
-		return T[total];
-	} 
-	
-	//in this version to make 3 - 1,2 and 2,1 are counted different.
-	//This is same as fibo series only that fibo series looks at last 2 numbers and here we look back k values
-	public int version2(int score[],int total){
-		int T[] = new int[total+1];
-		T[0] = 1;
-		for(int i=1; i <= total ; i++){
-			for(int j=0; j < score.length; j++){
-				if(score[j] <= i){
-					T[i] += T[i-score[j]];
-				}
-			}
-		}
-		return T[total];
-	}
-	
-	public static void main(String args[]){
-		int score[] = {1,2};
-		NumberOfWaysToScorePoints now = new NumberOfWaysToScorePoints();
-		System.out.println(now.version1(score, 4));
-		System.out.println(now.version2(score, 4));
-	}
+    //in this version to make 3 - 1,2 is same as 2,1.
+    //This is similar to coin changing problem
+    public int version1(int score[],int total){
+        int T[] = new int[total+1];
+        T[0] = 1;
+        for(int i=0; i < score.length; i++){
+            for(int j=1; j <= total; j++){
+                if(score[i] <= j){
+                    T[j] += T[j-score[i]];
+                }
+            }
+        }
+        return T[total];
+    } 
+    
+    //in this version to make 3 - 1,2 and 2,1 are counted different.
+    //This is same as fibo series only that fibo series looks at last 2 numbers and here we look back k values
+    public int version2(int score[],int total){
+        int T[] = new int[total+1];
+        T[0] = 1;
+        for(int i=1; i <= total ; i++){
+            for(int j=0; j < score.length; j++){
+                if(score[j] <= i){
+                    T[i] += T[i-score[j]];
+                }
+            }
+        }
+        return T[total];
+    }
+    
+    public static void main(String args[]){
+        int score[] = {1,2};
+        NumberOfWaysToScorePoints now = new NumberOfWaysToScorePoints();
+        System.out.println(now.version1(score, 4));
+        System.out.println(now.version2(score, 4));
+    }
 }
