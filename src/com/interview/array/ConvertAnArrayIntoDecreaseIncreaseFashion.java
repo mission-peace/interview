897360b24df551e8fc681eca57db5069033d737a
Roy <tusroy@amazon.com>
2015-02-13 09:55:14 -0800

    Adding easy method for increasing decreasing array question

--- src/com/interview/array/ConvertAnArrayIntoDecreaseIncreaseFashion.java
+++ src/com/interview/array/ConvertAnArrayIntoDecreaseIncreaseFashion.java
@@ -1,5 +1,7 @@
 package com.interview.array;
 
+import java.util.Arrays;
+
 
 /**
  * Convert an unsorted array into an array such that
@@ -27,6 +29,20 @@ public class ConvertAnArrayIntoDecreaseIncreaseFashion {
         
     }
     
+    /**
+     * Sort the array first.
+     * Then swap every adjacent element to get final result
+     * @param arr
+     */
+    public void convert1(int arr[]){
+        Arrays.sort(arr);
+        for(int i=1; i < arr.length; i+=2){
+            if(i+1 < arr.length){
+                swap(arr, i, i+1);
+            }
+        }
+    }
+    
     private void swap(int arr[],int low,int high){
         int temp = arr[low];
         arr[low] = arr[high];
@@ -39,6 +55,12 @@ public class ConvertAnArrayIntoDecreaseIncreaseFashion {
         can.convert(arr);
         for(int i=0; i < arr.length; i++){
             System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        int arr1[] = {0,6,9,13,10,-1,8,2,4,14,-5};
+        can.convert1(arr1);
+        for(int i=0; i < arr1.length; i++){
+            System.out.print(arr1[i] + " ");
         }
     }
 
