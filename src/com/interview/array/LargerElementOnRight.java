1e90b4785612f7a1f5b4a00e4ecf0e1182eb7c95
Roy <tusroy@amazon.com>
2015-02-14 13:56:18 -0800

    Adding comments

--- src/com/interview/array/LargerElementOnRight.java
+++ src/com/interview/array/LargerElementOnRight.java
@@ -7,6 +7,10 @@ import java.util.LinkedList;
  * http://www.geeksforgeeks.org/next-greater-element/
  * Find next larger element on right side of a number in array for 
  * all positions in array
+ * This is different than finding largest element on right side which can 
+ * be done by keeping max while iterating from right
+ * It is also different from find next higher number on right side which can
+ * be found by keeping AVL tree and finding ceiling.
  */
 public class LargerElementOnRight {
 
