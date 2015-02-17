64c3ea77bca4b7b91e548d12d2bd629e13efac52
Roy <tusroy@amazon.com>
2015-02-16 19:14:30 -0800

    Fixing tabs

--- src/com/interview/string/LongestSubstringWithoutRepetingCharacter.java
+++ src/com/interview/string/LongestSubstringWithoutRepetingCharacter.java
@@ -12,35 +12,35 @@ import java.util.Set;
  */
 public class LongestSubstringWithoutRepetingCharacter {
 
-	public int longestSubstring(char str[]){
-		int maxLen = -1 ;
-		int len =0;
-		int start=0;
-		Set<Character> visited = new HashSet<Character>();
-		for(int i=0; i < str.length; i++){
-			//if visited contains current character 
-			//remove all characters from start till previous version of current
-			//character if found.
-			if(visited.contains(str[i])){
-				if(len > maxLen){
-					maxLen = len;
-				}
-				do{
-					visited.remove(str[start]);
-					start++;
-					len--;
-				}while(str[start-1] != str[i]);
-			}
-			//this needs to be done always no matter character is found in visisted
-			//or not
-			visited.add(str[i]);
-			len++;
-		}
-		return Math.max(len, maxLen);
-	}
-	
-	public static void main(String args[]){
-		LongestSubstringWithoutRepetingCharacter lsw = new LongestSubstringWithoutRepetingCharacter();
-		System.out.println(lsw.longestSubstring("ABCDECAMNCZB".toCharArray()));
-	}
+    public int longestSubstring(char str[]){
+        int maxLen = -1 ;
+        int len =0;
+        int start=0;
+        Set<Character> visited = new HashSet<Character>();
+        for(int i=0; i < str.length; i++){
+            //if visited contains current character 
+            //remove all characters from start till previous version of current
+            //character if found.
+            if(visited.contains(str[i])){
+                if(len > maxLen){
+                    maxLen = len;
+                }
+                do{
+                    visited.remove(str[start]);
+                    start++;
+                    len--;
+                }while(str[start-1] != str[i]);
+            }
+            //this needs to be done always no matter character is found in visisted
+            //or not
+            visited.add(str[i]);
+            len++;
+        }
+        return Math.max(len, maxLen);
+    }
+    
+    public static void main(String args[]){
+        LongestSubstringWithoutRepetingCharacter lsw = new LongestSubstringWithoutRepetingCharacter();
+        System.out.println(lsw.longestSubstring("ABCDECAMNCZB".toCharArray()));
+    }
 }
