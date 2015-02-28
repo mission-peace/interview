10b182110df94831d6e36d1ca5feb663736d69e4
Roy <tusroy@amazon.com>
2015-02-28 15:38:06 -0800

    New method to calculate square of number without using multiplication method

--- src/com/interview/bits/SquareOfNumber.java
+++ src/com/interview/bits/SquareOfNumber.java
@@ -17,7 +17,7 @@ public class SquareOfNumber {
         }
         int result = 0;
         int k = n;
-        //saw we doing 7*7. First lets get rid of 3 from that 7 by below opeeration.
+        //so we doing 7*7. First lets get rid of 3 from that 7 by below operation.
         //Once we are left with 4 then it all about using >> and <<
         while((k & (k-1))!= 0){
             result += n;
@@ -31,12 +31,34 @@ public class SquareOfNumber {
         return result + n;
     }
     
+    public int fastSquare(int n){
+        return fastSquareRec(n, n);
+    }
+    /**
+     * Start with 9,9. Then take 1 and keep left shifting 1 till you find number
+     * less than 9 but power of 2. Then shift 9 by that many powers and repeat
+     * the process with whatever is left between that number and 9.
+    */
+    private int fastSquareRec(int n, int leftToMultiply){
+        if(leftToMultiply <= 0){
+            return 0;
+        }
+        int k = 1; 
+        int count=0;
+        while(k <= leftToMultiply){
+            k = k<<1;
+            count++;
+        }
+        k = k>>1;
+        count--;
+        return (n<<count) + fastSquareRec(n, leftToMultiply- k);
+    }
+    
     public static void main(String args[]){
         SquareOfNumber sn = new SquareOfNumber();
-        System.out.println(sn.square(9));
-        System.out.println(sn.square(7));
-        System.out.println(sn.square(6));
-        System.out.println(sn.square(8));
-        System.out.println(sn.square(14));
+        System.out.println(sn.square(9) + " " + sn.fastSquare(9));
+        System.out.println(sn.square(27) + " " + sn.fastSquare(27));
+        System.out.println(sn.square(18) + " " + sn.fastSquare(18));
+        System.out.println(sn.square(199) + " " + sn.fastSquare(199));
     }
 }
