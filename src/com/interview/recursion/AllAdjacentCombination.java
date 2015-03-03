694fb5514dd0bb309c8e72d5567c81163f086913
Roy <tusroy@amazon.com>
2015-03-02 19:01:54 -0800

    Fixing tab

--- src/com/interview/recursion/AllAdjacentCombination.java
+++ src/com/interview/recursion/AllAdjacentCombination.java
@@ -13,35 +13,33 @@ package com.interview.recursion;
  */
 public class AllAdjacentCombination {
 
-	public void combination(int input[],int result[],int k,int pos,int r){
-		
-		if(pos == input.length){
-			for(int i=0; i < r ; i++){
-				System.out.print(result[i] + " ");
-			}
-			System.out.println();
-			return;
-		}
-		for(int i=pos; i < pos + k && i < input.length; i++ ){
-			result[r] = formNumber(input,pos,i);
-			combination(input,result,k,i+1,r+1);
-		}
-	}
-	
-	private int formNumber(int input[],int start, int end){
-		int pow = 1;
-		int num = 0;
-		for(int j = end; j >= start; j--){
-			num += pow*input[j];
-			pow = pow*10;
-		}
-		return num;
-	}
-	
-	public static void main(String args[]){
-		AllAdjacentCombination adc = new AllAdjacentCombination();
-		int input[] = {1,2,3,4,5};
-		int result[] = new int[input.length];
-		adc.combination(input,result,3,0,0);
-	}
+    public void combination(int input[],int result[],int k,int pos,int r){
+        
+        if(pos == input.length){
+            for(int i=0; i < r ; i++){
+                System.out.print(result[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for(int i=pos; i < pos + k && i < input.length; i++ ){
+            result[r] = formNumber(input,pos,i);
+            combination(input,result,k,i+1,r+1);
+        }
+    }
+    
+    private int formNumber(int input[], int start, int end){
+        int num = 0;
+        for(int i=start; i <=end; i++){
+            num = num*10 + input[i];
+        }
+        return num;
+    }
+     
+    public static void main(String args[]){
+        AllAdjacentCombination adc = new AllAdjacentCombination();
+        int input[] = {1,2,3,4,5};
+        int result[] = new int[input.length];
+        adc.combination(input,result,3,0,0);
+    }
 }
