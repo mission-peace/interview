f3f68f5513e7d6264d3a1230248b461eb8eb8838
Roy <tusroy@amazon.com>
2015-03-04 19:11:38 -0800

    Fixing solution for trailing 0s

--- src/com/interview/number/Trailing0sinFactorial.java
+++ src/com/interview/number/Trailing0sinFactorial.java
@@ -6,24 +6,13 @@ package com.interview.number;
 public class Trailing0sinFactorial {
 
     public int trailing0s(int num){
-        
-        int total = num/5;
-        
-        int pow = 1;
+        int pow = 5;
+        int count = 0;
         while(pow <= num){
-            pow = pow*5;
+            count += num/pow;
+            pow *= 5;
         }
-        
-        pow /= 5;
-        
-        int index = 1;
-        while(pow > 5){
-            total = total + index;
-            index++;
-            pow = pow/5;
-        }
-        
-        return total;
+        return count;
     }
     
     public static void main(String args[]){
@@ -31,7 +20,6 @@ public class Trailing0sinFactorial {
         System.out.println(t0.trailing0s(625));
         System.out.println(t0.trailing0s(146));
         System.out.println(t0.trailing0s(1046));
-        System.out.println(t0.trailing0s(46));
-    
+        System.out.println(t0.trailing0s(4617));
     }
 }
