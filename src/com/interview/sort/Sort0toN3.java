6d2b6dca5eb6cb5832ca03001251bfa85c9de877
Roy <tusroy@amazon.com>
2015-03-10 09:23:34 -0700

    Fixing tab

--- src/com/interview/sort/Sort0toN3.java
+++ src/com/interview/sort/Sort0toN3.java
@@ -5,41 +5,41 @@ package com.interview.sort;
  */
 public class Sort0toN3 {
 
-	public void sort(int arr[],int n){
-		
-		sort(arr,n,1);
-		sort(arr,n,n);
-		sort(arr,n,n*n);
-	}
-	
-	private void sort(int arr[],int n, int exp){
-		int count[] = new int[n];
-		for(int i=0; i < arr.length;i++){
-			count[(arr[i]/exp)%n]++;
-		}
-		
-		for(int i=1; i < arr.length; i++){
-			count[i] += count[i-1];
- 		}
-	
-		int output[] = new int[n];
-		
-		for(int i=arr.length-1;i>=0; i--){
-			output[count[(arr[i]/exp)%n]-1] = arr[i];
-			count[(arr[i]/exp)%n]--;
-		}
-		for(int i=0; i < arr.length; i++){
-			arr[i] = output[i];
-		}
-	}
-	
-	public static void main(String args[]){
-		int arr[] = {100,2,124,18,36};
-		Sort0toN3 sn = new Sort0toN3();
-		sn.sort(arr,arr.length);
-		for(int i=0; i < arr.length; i++){
-			System.out.print(arr[i] + " ");
-		}
-	}
-	
+    public void sort(int arr[],int n){
+        
+        sort(arr,n,1);
+        sort(arr,n,n);
+        sort(arr,n,n*n);
+    }
+    
+    private void sort(int arr[],int n, int exp){
+        int count[] = new int[n];
+        for(int i=0; i < arr.length;i++){
+            count[(arr[i]/exp)%n]++;
+        }
+        
+        for(int i=1; i < arr.length; i++){
+            count[i] += count[i-1];
+        }
+    
+        int output[] = new int[n];
+        
+        for(int i=arr.length-1;i>=0; i--){
+            output[count[(arr[i]/exp)%n]-1] = arr[i];
+            count[(arr[i]/exp)%n]--;
+        }
+        for(int i=0; i < arr.length; i++){
+            arr[i] = output[i];
+        }
+    }
+    
+    public static void main(String args[]){
+        int arr[] = {100,2,124,18,36};
+        Sort0toN3 sn = new Sort0toN3();
+        sn.sort(arr,arr.length);
+        for(int i=0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    
 }
