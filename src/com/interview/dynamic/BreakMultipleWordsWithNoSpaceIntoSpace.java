0a6249fa9c02f4fc14a424537d052d0a04a0e710
Roy <tusroy@amazon.com>
2015-03-26 20:27:53 -0700

    Adding comments

--- src/com/interview/dynamic/BreakMultipleWordsWithNoSpaceIntoSpace.java
+++ src/com/interview/dynamic/BreakMultipleWordsWithNoSpaceIntoSpace.java
@@ -3,10 +3,37 @@ package com.interview.dynamic;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Date 08/01/2014
+ * @author tusroy
+ * 
+ * Given a string and a dictionary, split this string into multiple words such that
+ * each word belongs in dictionary.
+ * 
+ * e.g peanutbutter -> pea nut butter
+ * e.g Iliketoplay -> I like to play
+ * 
+ * Solution 
+ * DP solution to this problem
+ * if( input[i...j] belongs in dictionary) T[i][j] = i
+ * else{
+ *     T[i][j] = k if T[i][k-1] != -1 && T[k][j] != -1
+ *     
+ * Test cases
+ * 1) Empty string
+ * 2) String where entire string is in dictionary
+ * 3) String which cannot be split into words which are in dictionary
+ * 3) String which can be split into words which are in dictionary    
+ *
+ */
 public class BreakMultipleWordsWithNoSpaceIntoSpace {
 
+    
+    /**
+     * Recursive and slow version of breaking word problem.
+     * If no words can be formed it returns null
+     */
     public String breakWord(char[] str,int low,Set<String> dictionary){
-        
         StringBuffer buff = new StringBuffer();
         for(int i= low; i < str.length; i++){
             buff.append(str[i]);
@@ -23,79 +50,34 @@ public class BreakMultipleWordsWithNoSpaceIntoSpace {
         return null;
     }
     
-    public String dynamic(char str[], Set<String> dictionary){
-        
-        int[][] T = new int[str.length][str.length];
-        for(int i=0; i < T.length; i++){
-            for(int j=0; j < T[i].length; j++){
-                if(i == j && dictionary.contains(str[i])){
-                    T[i][j] = i;
-                }else{
-                    T[i][j] = -1;
-                }
-            }
-        }
-        StringBuffer buff = new StringBuffer();
-        
-        for(int l = 2; l <= str.length; l++){
-            for(int i = 0; i <= str.length-l; i++){
-                int j = i + l-1;
-                buff.append(str[i]);
-                int k;
-                for(k=i+1; k <=j; k++){
-                    if(dictionary.contains(buff.toString()) && T[k][j] != -1){
-            //          T[i][j] = k;
-                        break;
-                    }
-                    buff.append(str[k]);
-                }
-                if(dictionary.contains(buff.toString())){
-                    T[i][j] = k;
-                }
-                buff.delete(0, buff.length());
-            }
-        }
-        int low =0;
-        int high = str.length-1;
-        StringBuffer buffer = new StringBuffer();
-        while(true){
-            int r = T[low][high];
-            if(r==-1){
-                return null;
-            }
-            buffer.append(get(str,low,r)).append(" ");
-            low = r;
-            if(low == str.length){
-                break;
-            }
-        }
-        return buffer.toString();
-    }
-    
     /**
-     * Easier implementation to understand
+     * Dynamic programming version for breaking word problem.
+     * It returns null string if string cannot be broken into multipe words
+     * such that each word is in dictionary.
      * Gives preference to longer words over splits
      * e.g peanutbutter with dict{pea nut butter peanut} it would result in
-     * peanut butter instead of pea nut butter. Above method does the opposite
-     * @return
+     * peanut butter instead of pea nut butter.
      */
     public String breakWordDP(String word, Set<String> dict){
         int T[][] = new int[word.length()][word.length()];
         
         for(int i=0; i < T.length; i++){
             for(int j=0; j < T[i].length ; j++){
-                T[i][j] = -1;
+                T[i][j] = -1; //-1 indicates string between i to j cannot be split
             }
         }
         
+        //fill up the matrix in bottom up manner
         for(int l = 1; l <= word.length(); l++){
             for(int i=0; i < word.length() -l + 1 ; i++){
                 int j = i + l-1;
                 String str = word.substring(i,j+1);
+                //if string between i to j is in dictionary T[i][j] is true
                 if(dict.contains(str)){
                     T[i][j] = i;
                     continue;
                 }
+                //find a k between i+1 to j such that T[i][k-1] && T[k][j] are both true 
                 for(int k=i+1; k <= j; k++){
                     if(T[i][k-1] != -1 && T[k][j] != -1){
                         T[i][j] = k;
@@ -107,6 +89,8 @@ public class BreakMultipleWordsWithNoSpaceIntoSpace {
         if(T[0][word.length()-1] == -1){
             return null;
         }
+        
+        //create space separate word from string is possible
         StringBuffer buffer = new StringBuffer();
         int i = 0; int j = word.length() -1;
         while(i < j){
@@ -123,14 +107,6 @@ public class BreakMultipleWordsWithNoSpaceIntoSpace {
     }
 
     
-    private String get(char[] str,int low,int high){
-        StringBuffer b = new StringBuffer();
-        for(int i=low; i < high; i++){
-            b.append(str[i]);
-        }
-        return b.toString();
-    }
-    
     public static void main(String args[]){
         Set<String> dictionary = new HashSet<String>();
         dictionary.add("I");
@@ -140,10 +116,8 @@ public class BreakMultipleWordsWithNoSpaceIntoSpace {
         dictionary.add("to");
         String str = "Ihadliketoplay";
         BreakMultipleWordsWithNoSpaceIntoSpace bmw = new BreakMultipleWordsWithNoSpaceIntoSpace();
-        String result = bmw.dynamic(str.toCharArray(), dictionary);
         String result1 = bmw.breakWordDP(str, dictionary);
         
-        System.out.println(result);
         System.out.print(result1);
     }
 }
