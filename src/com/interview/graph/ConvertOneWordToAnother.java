60e7b1cf544e77406d8df14b334a14179060a65e
Roy <tusroy@amazon.com>
2015-03-29 21:02:28 -0700

    Fixing tab with space

--- src/com/interview/graph/ConvertOneWordToAnother.java
+++ src/com/interview/graph/ConvertOneWordToAnother.java
@@ -14,56 +14,56 @@ import java.util.Set;
  */
 public class ConvertOneWordToAnother {
 
-	public void convert(String word1, String word2, Set<String> dict) {
+    public void convert(String word1, String word2, Set<String> dict) {
 
-		Set<String> visited = new HashSet<String>();
-		Queue<String> queue = new LinkedList<String>();
-		Map<String, String> parentMap = new HashMap<String, String>();
-		queue.add(word1);
-		visited.add(word1);
-		while (queue.size() != 0) {
-			String word = queue.poll();
-			if (word.equals(word2)) {
-				break;
-			}
-			for (int i = 0; i < word.length(); i++) {
-				for (int ch = 'a'; ch <= 'z'; ch++) {
-					String newWord = replace(word, i, (char) ch);
-					if (dict.contains(newWord)){
-						if (!visited.contains(newWord)) {
-							parentMap.put(newWord, word);
-							queue.add(newWord);
-							visited.add(newWord);
-						}
-					}
-				}
-			}
-		}
-		String word = word2;
-		while(word != null){
-			System.out.println(word);
-			word = parentMap.get(word);
-		}
+        Set<String> visited = new HashSet<String>();
+        Queue<String> queue = new LinkedList<String>();
+        Map<String, String> parentMap = new HashMap<String, String>();
+        queue.add(word1);
+        visited.add(word1);
+        while (queue.size() != 0) {
+            String word = queue.poll();
+            if (word.equals(word2)) {
+                break;
+            }
+            for (int i = 0; i < word.length(); i++) {
+                for (int ch = 'a'; ch <= 'z'; ch++) {
+                    String newWord = replace(word, i, (char) ch);
+                    if (dict.contains(newWord)){
+                        if (!visited.contains(newWord)) {
+                            parentMap.put(newWord, word);
+                            queue.add(newWord);
+                            visited.add(newWord);
+                        }
+                    }
+                }
+            }
+        }
+        String word = word2;
+        while(word != null){
+            System.out.println(word);
+            word = parentMap.get(word);
+        }
 
-	}
+    }
 
-	public String replace(String newWord, int pos, char ch) {
-		StringBuffer buffer = new StringBuffer();
-		buffer.append(newWord.substring(0, pos));
-		buffer.append(ch);
-		buffer.append(newWord.substring(pos+1,newWord.length()));
-		return buffer.toString();
-	}
+    public String replace(String newWord, int pos, char ch) {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append(newWord.substring(0, pos));
+        buffer.append(ch);
+        buffer.append(newWord.substring(pos+1,newWord.length()));
+        return buffer.toString();
+    }
 
-	
-	public static void main(String args[]){
-		Set<String> dictionary = new HashSet<String>();
-		dictionary.add("cat");
-		dictionary.add("pat");
-		dictionary.add("pit");
-		dictionary.add("pid");
-		dictionary.add("did");
-		ConvertOneWordToAnother cow = new ConvertOneWordToAnother();
-		cow.convert("cat", "did", dictionary);
-	}
+    
+    public static void main(String args[]){
+        Set<String> dictionary = new HashSet<String>();
+        dictionary.add("cat");
+        dictionary.add("pat");
+        dictionary.add("pit");
+        dictionary.add("pid");
+        dictionary.add("did");
+        ConvertOneWordToAnother cow = new ConvertOneWordToAnother();
+        cow.convert("cat", "did", dictionary);
+    }
 }
--- src/com/interview/graph/StronglyConnectedComponent.java
+++ src/com/interview/graph/StronglyConnectedComponent.java
@@ -14,79 +14,79 @@ import java.util.Set;
  */
 public class StronglyConnectedComponent {
 
-	public List<Set<Vertex<Integer>>> scc(Graph<Integer> graph) {
-		Deque<Vertex<Integer>> stack = new ArrayDeque<Vertex<Integer>>();
-		Map<Vertex<Integer>, Boolean> visited = new HashMap<Vertex<Integer>, Boolean>();
-		for (Vertex<Integer> vertex : graph.getAllVertex()) {
-			if (visited.containsKey(vertex)) {
-				continue;
-			}
-			DFSUtil(vertex, visited, stack);
-		}
+    public List<Set<Vertex<Integer>>> scc(Graph<Integer> graph) {
+        Deque<Vertex<Integer>> stack = new ArrayDeque<Vertex<Integer>>();
+        Map<Vertex<Integer>, Boolean> visited = new HashMap<Vertex<Integer>, Boolean>();
+        for (Vertex<Integer> vertex : graph.getAllVertex()) {
+            if (visited.containsKey(vertex)) {
+                continue;
+            }
+            DFSUtil(vertex, visited, stack);
+        }
 
-		System.out.println(stack);
-		
-		Graph<Integer> reverseGraph = new Graph<Integer>(true);
-		Map<Long, Vertex<Integer>> vertexMap = new HashMap<Long, Vertex<Integer>>();
-		for (Edge<Integer> edge : graph.getAllEdges()) {
-			reverseGraph.addEdge(edge.getVertex2().getId(), edge.getVertex1()
-					.getId(), edge.getWeight());
-		}
-		
-		for (Vertex<Integer> vertex : reverseGraph.getAllVertex()) {
-			vertexMap.put(vertex.getId(), vertex);
-		}
-		
-		visited.clear();
-		List<Set<Vertex<Integer>>> result = new ArrayList<Set<Vertex<Integer>>>();
-		while (!stack.isEmpty()) {
-			Vertex<Integer> vertex = vertexMap.get(stack.poll().getId());
-			if(visited.containsKey(vertex)){
-				continue;
-			}
-			Set<Vertex<Integer>> set = new HashSet<Vertex<Integer>>();
-			DFSUtil1(vertex, visited, set);
-			result.add(set);
-		}
-		return result;
-	}
+        System.out.println(stack);
+        
+        Graph<Integer> reverseGraph = new Graph<Integer>(true);
+        Map<Long, Vertex<Integer>> vertexMap = new HashMap<Long, Vertex<Integer>>();
+        for (Edge<Integer> edge : graph.getAllEdges()) {
+            reverseGraph.addEdge(edge.getVertex2().getId(), edge.getVertex1()
+                    .getId(), edge.getWeight());
+        }
+        
+        for (Vertex<Integer> vertex : reverseGraph.getAllVertex()) {
+            vertexMap.put(vertex.getId(), vertex);
+        }
+        
+        visited.clear();
+        List<Set<Vertex<Integer>>> result = new ArrayList<Set<Vertex<Integer>>>();
+        while (!stack.isEmpty()) {
+            Vertex<Integer> vertex = vertexMap.get(stack.poll().getId());
+            if(visited.containsKey(vertex)){
+                continue;
+            }
+            Set<Vertex<Integer>> set = new HashSet<Vertex<Integer>>();
+            DFSUtil1(vertex, visited, set);
+            result.add(set);
+        }
+        return result;
+    }
 
-	private void DFSUtil(Vertex<Integer> vertex,
-			Map<Vertex<Integer>, Boolean> visited, Deque<Vertex<Integer>> stack) {
-		visited.put(vertex, true);
-		for (Vertex<Integer> v : vertex.getAdjacentVertexes()) {
-			if (visited.containsKey(v)) {
-				continue;
-			}
-			DFSUtil(v, visited, stack);
-		}
-		stack.offerFirst(vertex);
-	}
+    private void DFSUtil(Vertex<Integer> vertex,
+            Map<Vertex<Integer>, Boolean> visited, Deque<Vertex<Integer>> stack) {
+        visited.put(vertex, true);
+        for (Vertex<Integer> v : vertex.getAdjacentVertexes()) {
+            if (visited.containsKey(v)) {
+                continue;
+            }
+            DFSUtil(v, visited, stack);
+        }
+        stack.offerFirst(vertex);
+    }
 
-	private void DFSUtil1(Vertex<Integer> vertex,
-			Map<Vertex<Integer>, Boolean> visited, Set<Vertex<Integer>> set) {
-		visited.put(vertex, true);
-		set.add(vertex);
-		for (Vertex<Integer> v : vertex.getAdjacentVertexes()) {
-			if (visited.containsKey(v)) {
-				continue;
-			}
-			DFSUtil1(v, visited, set);
-		}
-	}
+    private void DFSUtil1(Vertex<Integer> vertex,
+            Map<Vertex<Integer>, Boolean> visited, Set<Vertex<Integer>> set) {
+        visited.put(vertex, true);
+        set.add(vertex);
+        for (Vertex<Integer> v : vertex.getAdjacentVertexes()) {
+            if (visited.containsKey(v)) {
+                continue;
+            }
+            DFSUtil1(v, visited, set);
+        }
+    }
 
-	public static void main(String args[]){
-		Graph<Integer> graph = new Graph<Integer>(true);
-		graph.addEdge(1, 0);
-		graph.addEdge(2,1);
-		graph.addEdge(0,2);
-		graph.addEdge(0, 3);
-		graph.addEdge(3, 4);
-		
-		StronglyConnectedComponent scc = new StronglyConnectedComponent();
-		List<Set<Vertex<Integer>>> result = scc.scc(graph);
-		for(Set<Vertex<Integer>> set : result){
-			System.out.println(set);
-		}
-	}
+    public static void main(String args[]){
+        Graph<Integer> graph = new Graph<Integer>(true);
+        graph.addEdge(1, 0);
+        graph.addEdge(2,1);
+        graph.addEdge(0,2);
+        graph.addEdge(0, 3);
+        graph.addEdge(3, 4);
+        
+        StronglyConnectedComponent scc = new StronglyConnectedComponent();
+        List<Set<Vertex<Integer>>> result = scc.scc(graph);
+        for(Set<Vertex<Integer>> set : result){
+            System.out.println(set);
+        }
+    }
 }
