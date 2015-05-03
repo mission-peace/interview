94e09f53831b181d010189fe359a85e2e9a4a691
Roy <tusroy@amazon.com>
2015-05-03 14:33:15 -0700

    Updating doc of fenwick tree

--- src/com/interview/tree/FenwickTree.java
+++ src/com/interview/tree/FenwickTree.java
@@ -21,29 +21,69 @@ package com.interview.tree;
  */
 public class FenwickTree {
 
+    /**
+     * Start from index+1 if you updating index in original array. Keep adding this value 
+     * for next node till you reach outside range of tree
+     * @param binaryIndexedTree
+     * @param val
+     * @param index
+     */
     public void updateBinaryIndexedTree(int binaryIndexedTree[], int val, int index){
         while(index < binaryIndexedTree.length){
             binaryIndexedTree[index] += val;
-            index += (index & -index);
+            index = getNext(index);
         }
     }
     
+    /**
+     * Start from index+1 if you want prefix sum 0 to index. Keep adding value
+     * till you reach 0
+     * @param binaryIndexedTree
+     * @param index
+     * @return
+     */
     public int getSum(int binaryIndexedTree[], int index){
         index = index + 1;
         int sum = 0;
         while(index > 0){
             sum += binaryIndexedTree[index];
-            index -= (index & -index);
+            index = getParent(index);
         }
         return sum;
     }
     
+    /**
+     * Creating tree is like updating Fenwick tree for every value in array
+     * @param input
+     * @return
+     */
     public int[] createTree(int input[]){
         int binaryIndexedTree[] = new int[input.length+1];
         for(int i=1; i <= input.length; i++){
             updateBinaryIndexedTree(binaryIndexedTree, input[i-1], i);
         }
         return binaryIndexedTree;
+    }
+    
+    /**
+     * To get parent
+     * 1) 2's complement to get minus of index
+     * 2) AND this with index
+     * 3) Subtract that from index
+     */
+    private int getParent(int index){
+        return index - (index & -index);
+    }
+    
+    /**
+     * To get next
+     * 1) 2's complement of get minus of index
+     * 2) AND this with index
+     * 3) Add it to index
+     * @return
+     */
+    private int getNext(int index){
+        return index + (index & -index);
     }
     
     public static void main(String args[]){
