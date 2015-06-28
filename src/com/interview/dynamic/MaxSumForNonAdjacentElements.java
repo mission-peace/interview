5e127cba669f14c9aecbe600bd0a3bfb3918c160
Ethan <castives@gmail.com>
2015-06-27 19:13:36 -0500

    adding a illustration for MaxSumForNonAdjacentElements

--- src/com/interview/dynamic/MaxSumForNonAdjacentElements.java
+++ src/com/interview/dynamic/MaxSumForNonAdjacentElements.java
@@ -23,3 +23,27 @@ public class MaxSumForNonAdjacentElements {
         System.out.println(msn.maxSum(arr));
     }
 }
+
+
+
+/**
+ * Another way to do it, a better illustration of the method used above
+ */
+public class MaxSumForNonAdjacentElements {	
+    
+    public int maxSum(int arr[], int index) {
+    	if (index==0){
+    		return arr[0];
+    	}else if(index == 1){
+    		return Math.max(arr[0],arr[1]);
+    	}	
+    	return Math.max(this.maxSum(arr, index-2)+arr[index],this.maxSum(arr, index-1) );
+    }
+
+    public static void main(String args[]) {
+        MaxSumForNonAdjacentElements msn = new MaxSumForNonAdjacentElements();
+        int arr[] = { 2, 10, 3, 4, 15, 10 };
+        System.out.println(msn.maxSum(arr,arr.length-1));
+    }
+}
+
