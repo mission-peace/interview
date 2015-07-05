5b90fd742080e1d798b466dbe32c8d51c3b82153
Tushar Roy <tushar_v_roy@Tushars-MacBook-Pro.local>
2015-07-05 16:09:13 -0700

    Adding recursive solution for max sum non adjacent

--- src/com/interview/dynamic/MaxSumForNonAdjacentElements.java
+++ src/com/interview/dynamic/MaxSumForNonAdjacentElements.java
@@ -6,6 +6,9 @@ package com.interview.dynamic;
  */
 public class MaxSumForNonAdjacentElements {
 
+	/**
+	 * Fast DP solution.
+	 */
     public int maxSum(int arr[]) {
         int excl = 0;
         int incl = arr[0];
@@ -16,10 +19,24 @@ public class MaxSumForNonAdjacentElements {
         }
         return incl;
     }
-
+    
+    /**
+     * Recursive slow solution.
+     */
+	public int maxSum(int arr[], int index) {
+		if (index == 0) {
+			return arr[0];
+		} else if (index == 1) {
+			return Math.max(arr[0], arr[1]);
+		}
+		return Math.max(this.maxSum(arr, index - 2) + arr[index], this.maxSum(arr, index - 1));
+	}    	
+   
     public static void main(String args[]) {
         MaxSumForNonAdjacentElements msn = new MaxSumForNonAdjacentElements();
-        int arr[] = { 2, 10, 3, 4, 15, 10 };
+        int arr[] = { 2, 10, 13, 4, 2, 15, 10 };
         System.out.println(msn.maxSum(arr));
+        System.out.println(msn.maxSum(arr, arr.length-1));
+                
     }
 }
