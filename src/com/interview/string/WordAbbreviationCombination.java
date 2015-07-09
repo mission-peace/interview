1b31201fad750f16356eae1b8846b0149bdfe042
Tushar Roy <tushar_v_roy@Tushars-MacBook-Pro.local>
2015-07-09 00:06:03 -0700

    Fix tab with space

--- src/com/interview/string/WordAbbreviationCombination.java
+++ src/com/interview/string/WordAbbreviationCombination.java
@@ -14,23 +14,23 @@ package com.interview.string;
  * hea1s
  */
 public class WordAbbreviationCombination {
-	public void shorten(char input[]){
-		for(int i = 1; i < input.length-1; i++){
-			for(int j=i; j < input.length-1; j++){
-				for(int k=0; k < i; k++){
-					System.out.print(input[k]);
-				}
-				System.out.print(j-i+1);
-				for(int k=j+1; k < input.length; k++){
-					System.out.print(input[k]);
-				}
-				System.out.println();
-			}
-		}
-	}
+    public void shorten(char input[]){
+        for(int i = 1; i < input.length-1; i++){
+            for(int j=i; j < input.length-1; j++){
+                for(int k=0; k < i; k++){
+                    System.out.print(input[k]);
+                }
+                System.out.print(j-i+1);
+                for(int k=j+1; k < input.length; k++){
+                    System.out.print(input[k]);
+                }
+                System.out.println();
+            }
+        }
+    }
 
-	public static void main(String args[]) {
-		WordAbbreviationCombination ssc = new WordAbbreviationCombination();
-		ssc.shorten("heading".toCharArray());
-	}
+    public static void main(String args[]) {
+        WordAbbreviationCombination ssc = new WordAbbreviationCombination();
+        ssc.shorten("heading".toCharArray());
+    }
 }
