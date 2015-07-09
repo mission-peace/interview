64d7d09b4f76d564e47d10b57ac2f02b0035bc39
Tushar Roy <tushar_v_roy@Tushars-MacBook-Pro.local>
2015-07-09 00:01:56 -0700

    Word abbreviation combination

---
+++ src/com/interview/string/WordAbbreviationCombination.java
@@ -0,0 +1,36 @@
+package com.interview.string;
+
+/**
+ * Date 07/08/2015
+ * @author tushar_v_roy
+ * 
+ * Given a string like heads print all combination of abbreviation
+ * Output should be
+ * h1ads
+ * h2ds
+ * h3s
+ * he1ds
+ * he2s
+ * hea1s
+ */
+public class WordAbbreviationCombination {
+	public void shorten(char input[]){
+		for(int i = 1; i < input.length-1; i++){
+			for(int j=i; j < input.length-1; j++){
+				for(int k=0; k < i; k++){
+					System.out.print(input[k]);
+				}
+				System.out.print(j-i+1);
+				for(int k=j+1; k < input.length; k++){
+					System.out.print(input[k]);
+				}
+				System.out.println();
+			}
+		}
+	}
+
+	public static void main(String args[]) {
+		WordAbbreviationCombination ssc = new WordAbbreviationCombination();
+		ssc.shorten("heading".toCharArray());
+	}
+}
