ee891509f659319ddda8ca448c12abf784186ada
Tushar Roy <tushar_v_roy@apple.com>
2015-08-02 18:29:05 -0700

    Adding docs on topological sort

--- src/com/interview/graph/TopologicalSort.java
+++ src/com/interview/graph/TopologicalSort.java
@@ -1,17 +1,31 @@
 package com.interview.graph;
 
-import java.util.ArrayDeque;
+import java.util.Set;
+import java.util.HashSet;
 import java.util.Deque;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayDeque;
 
+/**
+ * Date 08/20/2014
+ * @author Tushar Roy
+ *
+ * Given a directed acyclic graph, do a topological sort on this graph.
+ *
+ * Do DFS by keeping visited. Put the vertex which are completely explored into a stack.
+ * Pop from stack to get sorted order.
+ *
+ * Space and time complexity is O(n).
+ */
 public class TopologicalSort<T> {
 
+    /**
+     * Main method to be invoked to do topological sorting.
+     */
     public Deque<Vertex<T>> topSort(Graph<T> graph) {
-        Deque<Vertex<T>> stack = new ArrayDeque<Vertex<T>>();
-        Map<Vertex<T>, Boolean> visited = new HashMap<Vertex<T>, Boolean>();
+        Deque<Vertex<T>> stack = new ArrayDeque<>();
+        Set<Vertex<T>> visited = new HashSet<>();
         for (Vertex<T> vertex : graph.getAllVertex()) {
-            if (visited.containsKey(vertex)) {
+            if (visited.contains(vertex)) {
                 continue;
             }
             topSortUtil(vertex,stack,visited);
@@ -20,10 +34,10 @@ public class TopologicalSort<T> {
     }
 
     private void topSortUtil(Vertex<T> vertex, Deque<Vertex<T>> stack,
-            Map<Vertex<T>, Boolean> visited) {
-        visited.put(vertex, true);
+            Set<Vertex<T>> visited) {
+        visited.add(vertex);
         for(Vertex<T> childVertex : vertex.getAdjacentVertexes()){
-            if(visited.containsKey(childVertex)){
+            if(visited.contains(childVertex)){
                 continue;
             }
             topSortUtil(childVertex,stack,visited);
@@ -32,7 +46,7 @@ public class TopologicalSort<T> {
     }
     
     public static void main(String args[]){
-        Graph<Integer> graph = new Graph<Integer>(true);
+        Graph<Integer> graph = new Graph<>(true);
         graph.addEdge(1, 3);
         graph.addEdge(1, 2);
         graph.addEdge(3, 4);
