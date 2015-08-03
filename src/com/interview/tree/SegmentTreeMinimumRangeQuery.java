b15fe4ed101256422f9a3992a5b0d8c6ba55ebba
Tushar Roy <tushar_v_roy@apple.com>
2015-08-02 23:28:50 -0700

    Doing range update for segment tree

--- src/com/interview/tree/SegmentTreeMinimumRangeQuery.java
+++ src/com/interview/tree/SegmentTreeMinimumRangeQuery.java
@@ -13,11 +13,14 @@ import com.interview.bits.NextPowerOf2;
  * createSegmentTree(int arr[]) - create segment tree
  * query(int segment[], int startRange, int endRange) - returns minimum between startRange and endRange
  * update(int input[], int segment[], int indexToBeUpdated, int newVal) - updates input and segmentTree with newVal at index indexToBeUpdated;
- * 
+ * updateRange(int input[], int segment[], int lowRange, int highRange, int newVal) - updates all the values in given range with
+ * given newVal.
+ *
  * Time complexity to create segment tree is O(n) since new array will be at max 4n size
  * Space complexity to create segment tree is O(n) since new array will be at max 4n size
  * Time complexity to search in segment tree is O(logn) since you would at max travel 4 depths
  * Time complexity to update in segment tree is O(logn)
+ * Time complexity to update range in segment tree is O(range)
  * 
  * References
  * http://www.geeksforgeeks.org/segment-tree-set-1-sum-of-given-range/
@@ -80,6 +83,29 @@ public class SegmentTreeMinimumRangeQuery {
         updateSegmentTree(segmentTree, index, newVal, mid+1, high, 2*pos + 2);
         segmentTree[pos] = Math.min(segmentTree[2*pos+1], segmentTree[2*pos + 2]);
     }
+
+    public void updateSegmentTreeRange(int input[], int segmentTree[], int startRange, int endRange, int newVal) {
+        for(int i = startRange; i <= endRange; i++) {
+            input[i] = newVal;
+        }
+        updateSegmentTreeRange(segmentTree, startRange, endRange, newVal, 0, input.length-1, 0);
+    }
+
+    private void updateSegmentTreeRange(int segmentTree[], int startRange, int endRange, int newVal, int low, int high, int pos) {
+        if(low > high || startRange > high || endRange < low ) {
+            return;
+        }
+
+        if(low == high) {
+            segmentTree[pos] = newVal;
+            return;
+        }
+
+        int middle = (low + high)/2;
+        updateSegmentTreeRange(segmentTree, startRange, endRange, newVal, low, middle, 2*pos + 1);
+        updateSegmentTreeRange(segmentTree, startRange, endRange, newVal, middle+1, high, 2*pos + 2);
+        segmentTree[pos] = Math.min(segmentTree[2*pos+1], segmentTree[2*pos+2]);
+    }
     
     
     public int rangeMinimumQuery(int []segmentTree,int qlow,int qhigh,int len){
@@ -117,5 +143,8 @@ public class SegmentTreeMinimumRangeQuery {
         assert -1 == st.rangeMinimumQuery(segTree, 1, 6, input.length);
         st.updateSegmentTree(input, segTree, 2, 1);
         assert 1 == st.rangeMinimumQuery(segTree, 1, 3, input.length);
+        st.updateSegmentTreeRange(input, segTree, 3, 5, -2);
+        assert -2 == st.rangeMinimumQuery(segTree, 5, 6, input.length);
+        assert -2 == st.rangeMinimumQuery(segTree, 0, 3, input.length);
     }
 }
