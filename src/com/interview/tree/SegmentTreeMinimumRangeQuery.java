8b9ad70e15105dfbd5a7a40bf5b7491ebd625aa3
Tushar Roy <tushar_v_roy@apple.com>
2015-08-12 00:51:48 -0700

    Fixing bugs in lazy propagation in segment tree

--- src/com/interview/tree/SegmentTreeMinimumRangeQuery.java
+++ src/com/interview/tree/SegmentTreeMinimumRangeQuery.java
@@ -44,7 +44,7 @@ public class SegmentTreeMinimumRangeQuery {
         for(int i=0; i < segmentTree.length; i++){
             segmentTree[i] = Integer.MAX_VALUE;
         }
-        constructMinSegmentTreeTree(segmentTree,input,0,input.length-1,0);
+        constructMinSegmentTree(segmentTree, input, 0, input.length - 1, 0);
         return segmentTree;
         
     }
@@ -88,14 +88,14 @@ public class SegmentTreeMinimumRangeQuery {
         return rangeMinimumQueryLazy(segmentTree, lazy, qlow, qhigh, 0, len - 1, 0);
     }
 
-    private void constructMinSegmentTreeTree(int segmentTree[], int input[], int low, int high,int pos){
+    private void constructMinSegmentTree(int segmentTree[], int input[], int low, int high,int pos){
         if(low == high){
             segmentTree[pos] = input[low];
             return;
         }
         int mid = (low + high)/2;
-        constructMinSegmentTreeTree(segmentTree,input,low,mid,2*pos+1);
-        constructMinSegmentTreeTree(segmentTree,input,mid+1,high,2*pos+2);
+        constructMinSegmentTree(segmentTree, input, low, mid, 2 * pos + 1);
+        constructMinSegmentTree(segmentTree, input, mid + 1, high, 2 * pos + 2);
         segmentTree[pos] = Math.min(segmentTree[2*pos+1], segmentTree[2*pos+2]);
     }
     
@@ -149,20 +149,30 @@ public class SegmentTreeMinimumRangeQuery {
                 rangeMinimumQuery(segmentTree, mid + 1, high, qlow, qhigh, 2 * pos + 2));
     }
 
-    private void updateSegmentTreeRangeLazy(int segmentTree[], int lazy[], int startRange, int endRange, int delta, int low, int high, int pos) {
-
-        if(low > high || startRange > high || endRange < low) {
+    private void updateSegmentTreeRangeLazy(int segmentTree[],
+                                            int lazy[], int startRange, int endRange,
+                                            int delta, int low, int high, int pos) {
+        if(low > high) {
             return;
         }
 
+        //make sure all propagation is done at pos. If not update tree
+        //at pos and mark its children for lazy propagation.
         if (lazy[pos] != 0) {
+            segmentTree[pos] += lazy[pos];
             if (low != high) { //not a leaf node
-                segmentTree[2 * pos + 1] += lazy[pos];
-                segmentTree[2 * pos + 2] += lazy[pos];
+                lazy[2 * pos + 1] += lazy[pos];
+                lazy[2 * pos + 2] += lazy[pos];
             }
             lazy[pos] = 0;
         }
 
+        //no overlap condition
+        if(startRange > high || endRange < low) {
+            return;
+        }
+
+        //total overlap condition
         if(startRange <= low && endRange >= high) {
             segmentTree[pos] += delta;
             if(low != high) {
@@ -172,61 +182,59 @@ public class SegmentTreeMinimumRangeQuery {
             return;
         }
 
+        //otherwise partial overlap so look both left and right.
         int mid = (low + high)/2;
-        updateSegmentTreeRangeLazy(segmentTree, lazy, startRange, endRange, delta, low, mid, 2*pos+1);
-        updateSegmentTreeRangeLazy(segmentTree, lazy, startRange, endRange, delta, mid+1, high, 2*pos+1);
+        updateSegmentTreeRangeLazy(segmentTree, lazy, startRange, endRange,
+                delta, low, mid, 2*pos+1);
+        updateSegmentTreeRangeLazy(segmentTree, lazy, startRange, endRange,
+                delta, mid+1, high, 2*pos+2);
         segmentTree[pos] = Math.min(segmentTree[2*pos + 1], segmentTree[2*pos + 2]);
     }
 
-    private int rangeMinimumQueryLazy(int segmentTree[], int lazy[], int qlow, int qhigh, int low, int high, int pos) {
+    private int rangeMinimumQueryLazy(int segmentTree[], int lazy[], int qlow, int qhigh,
+                                      int low, int high, int pos) {
 
-        if(qlow > high || qhigh < low){
+        if(low > high) {
             return Integer.MAX_VALUE;
         }
 
+        //make sure all propagation is done at pos. If not update tree
+        //at pos and mark its children for lazy propagation.
         if (lazy[pos] != 0) {
             segmentTree[pos] += lazy[pos];
             if (low != high) { //not a leaf node
-                segmentTree[2 * pos + 1] += lazy[pos];
-                segmentTree[2 * pos + 2] += lazy[pos];
+                lazy[2 * pos + 1] += lazy[pos];
+                lazy[2 * pos + 2] += lazy[pos];
             }
             lazy[pos] = 0;
         }
 
+        //no overlap
+        if(qlow > high || qhigh < low){
+            return Integer.MAX_VALUE;
+        }
+
+        //total overlap
         if(qlow <= low && qhigh >= high){
             return segmentTree[pos];
         }
+
+        //partial overlap
         int mid = (low+high)/2;
-        return Math.min(rangeMinimumQuery(segmentTree, low, mid, qlow, qhigh, 2 * pos + 1),
-                rangeMinimumQuery(segmentTree, mid + 1, high, qlow, qhigh, 2 * pos + 2));
+        return Math.min(rangeMinimumQueryLazy(segmentTree, lazy, qlow, qhigh,
+                        low, mid, 2 * pos + 1),
+                rangeMinimumQueryLazy(segmentTree, lazy,  qlow, qhigh,
+                        mid + 1, high, 2 * pos + 2));
 
     }
 
-    /**
-     * Segment tree for given example
-     *                         -1
-     *                0                -1
-     *            0       2         1         -1
-     *         0    3   4   2     1   6    max   max 
-     * 
-     */
-    
     public static void main(String args[]){
         SegmentTreeMinimumRangeQuery st = new SegmentTreeMinimumRangeQuery();
-        int input[] = {0,3,4,2,1,6,-1};
+        int input[] = {-1,2,4,1,7,1,3,2};
         int segTree[] = st.createSegmentTree(input);
-        
-        assert 0 == st.rangeMinimumQuery(segTree, 0, 3, input.length);
-        assert 1 == st.rangeMinimumQuery(segTree, 1, 5, input.length);
-        assert -1 == st.rangeMinimumQuery(segTree, 1, 6, input.length);
-        st.updateSegmentTree(input, segTree, 2, 1);
-        assert 2 == st.rangeMinimumQuery(segTree, 1, 3, input.length);
-        st.updateSegmentTreeRange(input, segTree, 3, 5, -2);
-        assert -1 == st.rangeMinimumQuery(segTree, 5, 6, input.length);
-        assert 0 == st.rangeMinimumQuery(segTree, 0, 3, input.length);
         int lazy[] =  new int[segTree.length];
-        st.updateSegmentTreeRangeLazy(input, segTree, lazy, 2, 5, 2);
-        assert -1 == st.rangeMinimumQueryLazy(segTree, lazy, 2, 5, input.length);
-
+        st.updateSegmentTreeRangeLazy(input, segTree, lazy, 0, 3, 1);
+        st.updateSegmentTreeRangeLazy(input, segTree, lazy, 0, 0, 2);
+        assert 1 == st.rangeMinimumQueryLazy(segTree, lazy, 3, 5, input.length);
     }
 }
