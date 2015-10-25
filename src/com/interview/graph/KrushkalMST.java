f062fe58cd421156ea98d272e26d75d7d7265bf8
Tushar Roy <tushar_v_roy@apple.com>
2015-10-24 21:57:08 -0700

    Kruskals algorithm update

--- src/com/interview/graph/KrushkalMST.java
+++ src/com/interview/graph/KrushkalMST.java
@@ -6,10 +6,22 @@ import java.util.Comparator;
 import java.util.List;
 
 /**
- http://www.geeksforgeeks.org/greedy-algorithms-set-2-kruskals-minimum-spanning-tree-mst/
+ * Date 09/25/2014
+ * @author Tushar Roy
+ *
+ * Find minimum spanning tree usinig Kruskals algorithm
+ *
+ * Time complexity - O(ElogE)
+ * Space complexity - O(E + V)
+ *
+ * References
+ * https://en.wikipedia.org/wiki/Kruskal%27s_algorithm
  */
 public class KrushkalMST {
 
+    /**
+     * Comparator to sort edges by weight in non decreasing order
+     */
     public class EdgeComparator implements Comparator<Edge<Integer>>{
 
         @Override
@@ -20,16 +32,17 @@ public class KrushkalMST {
                 return 1;
             }
         }
-        
     }
-    
+
     public List<Edge<Integer>> getMST(Graph<Integer> graph){
-        
         List<Edge<Integer>> allEdges = graph.getAllEdges();
         EdgeComparator edgeComparator = new EdgeComparator();
+
+        //sort all edges in non decreasing order
         Collections.sort(allEdges, edgeComparator);
         DisjointSet disjointSet = new DisjointSet();
-        
+
+        //create as many disjoint sets as the total vertices
         for(Vertex<Integer> vertex : graph.getAllVertex()){
             disjointSet.makeSet(vertex.getId());
         }
@@ -37,11 +50,16 @@ public class KrushkalMST {
         List<Edge<Integer>> resultEdge = new ArrayList<Edge<Integer>>();
         
         for(Edge<Integer> edge : allEdges){
+            //get the sets of two vertices of the edge
             long root1 = disjointSet.findSet(edge.getVertex1().getId());
             long root2 = disjointSet.findSet(edge.getVertex2().getId());
+
+            //check if the vertices are in same set or different set
+            //if verties are in same set then ignore the edge
             if(root1 == root2){
                 continue;
             }else{
+                //if vertices are in different set then add the edge to result and union these two sets into one
                 resultEdge.add(edge);
                 disjointSet.union(edge.getVertex1().getId(), edge.getVertex2().getId());
             }
@@ -49,7 +67,7 @@ public class KrushkalMST {
         }
         return resultEdge;
     }
-    
+
     public static void main(String args[]){
         
         Graph<Integer> graph = new Graph<Integer>(false);
