fea3fecce1ee49b52a9e9d8cf7b7ad4d3675fe25
Tushar Roy <tushar_v_roy@apple.com>
2015-11-12 00:37:38 -0800

    Added red black tree delete

--- src/com/interview/tree/RedBlackTree.java
+++ src/com/interview/tree/RedBlackTree.java
@@ -1,6 +1,9 @@
 package com.interview.tree;
 
+import javax.swing.*;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Date 10/29/2015
@@ -30,13 +33,24 @@ public class RedBlackTree {
         Node left;
         Node right;
         Node parent;
+        boolean isNullLeaf;
     }
 
     private static Node createBlackNode(int data) {
         Node node = new Node();
         node.data = data;
         node.color = Color.BLACK;
+        node.left = createNullLeafNode(node);
+        node.right = createNullLeafNode(node);
         return node;
+    }
+
+    private static Node createNullLeafNode(Node parent) {
+        Node leaf = new Node();
+        leaf.color = Color.BLACK;
+        leaf.isNullLeaf = true;
+        leaf.parent = parent;
+        return leaf;
     }
 
     private static Node createRedNode(Node parent, int data) {
@@ -44,9 +58,10 @@ public class RedBlackTree {
         node.data = data;
         node.color = Color.RED;
         node.parent = parent;
+        node.left = createNullLeafNode(node);
+        node.right = createNullLeafNode(node);
         return node;
     }
-
 
     private void rightRotate(Node root, boolean changeColor) {
         Node parent = root.parent;
@@ -94,12 +109,12 @@ public class RedBlackTree {
         }
     }
 
-    private Optional<Node> findSiblingColor(Node root) {
+    private Optional<Node> findSiblingNode(Node root) {
         Node parent = root.parent;
         if(isLeftChild(root)) {
-            return Optional.ofNullable(parent.right);
+            return Optional.ofNullable(parent.right.isNullLeaf ? null : parent.right);
         } else {
-            return Optional.ofNullable(parent.left);
+            return Optional.ofNullable(parent.left.isNullLeaf ? null : parent.left);
         }
     }
 
@@ -117,7 +132,7 @@ public class RedBlackTree {
     }
 
     private Node insert(Node parent, Node root, int data) {
-        if(root  == null) {
+        if(root  == null || root.isNullLeaf) {
             //if parent is not null means tree is not empty
             //so create a red leaf node
             if(parent != null) {
@@ -164,7 +179,7 @@ public class RedBlackTree {
             if(root.color == Color.RED && root.left.color == Color.RED) {
                 //get the sibling of root. It is returning optional means
                 //sibling could be empty
-                Optional<Node> sibling = findSiblingColor(root);
+                Optional<Node> sibling = findSiblingNode(root);
                 //if sibling is empty or of BLACK color
                 if(!sibling.isPresent() || sibling.get().color == Color.BLACK) {
                     //check if root is left child of its parent
@@ -202,7 +217,7 @@ public class RedBlackTree {
         } else {
             //this is mirror case of above. So same comments as above.
             if(root.color == Color.RED && root.right.color == Color.RED) {
-                Optional<Node> sibling = findSiblingColor(root);
+                Optional<Node> sibling = findSiblingNode(root);
                 if(!sibling.isPresent() || sibling.get().color == Color.BLACK) {
                     if(!isLeftChild(root)) {
                         leftRotate(root, true);
@@ -223,8 +238,148 @@ public class RedBlackTree {
         return root;
     }
 
+    public Node delete(Node root, int data) {
+        AtomicReference<Node> rootReference = new AtomicReference<>();
+        delete(root, data, rootReference);
+        if(rootReference.get() == null) {
+            return root;
+        } else {
+            return rootReference.get();
+        }
+    }
+
+    private void delete(Node root, int data, AtomicReference<Node> rootReference) {
+        if(root == null || root.isNullLeaf) {
+            return;
+        }
+        if(root.data == data) {
+            if(root.right.isNullLeaf || root.left.isNullLeaf) {
+                deleteOneChild(root, rootReference);
+            }
+            Node inorderSuccessor = findSmallest(root.right);
+            root.data = inorderSuccessor.data;
+            delete(root.right, inorderSuccessor.data, rootReference);
+        }
+        if(root.data < data) {
+            delete(root.right, data, rootReference);
+        } else {
+            delete(root.left, data, rootReference);
+        }
+    }
+
+    private Node findSmallest(Node root) {
+        Node prev = null;
+        while(root != null && root.isNullLeaf) {
+            prev = root;
+            root = root.left;
+        }
+        return prev != null ? prev : root;
+    }
+
+    private void deleteOneChild(Node nodeToBeDelete, AtomicReference<Node> rootReference) {
+        Node child = nodeToBeDelete.right.isNullLeaf ? nodeToBeDelete.left : nodeToBeDelete.right;
+        replaceNode(nodeToBeDelete, child, rootReference);
+        if(nodeToBeDelete.color == Color.BLACK) {
+            if(child.color == Color.RED) {
+                child.color = Color.BLACK;
+            } else {
+                deleteCase1(child, rootReference);
+            }
+        }
+    }
+
+
+    private void deleteCase1(Node doubleBlackNode, AtomicReference<Node> rootReference) {
+        if(doubleBlackNode.parent == null) {
+            rootReference.set(doubleBlackNode);
+            return;
+        }
+        deleteCase2(doubleBlackNode, rootReference);
+    }
+
+    private void deleteCase2(Node doubleBlackNode, AtomicReference<Node> rootReference) {
+        Node siblingNode = findSiblingNode(doubleBlackNode).get();
+        if(siblingNode.color == Color.RED) {
+            if(isLeftChild(siblingNode)) {
+                rightRotate(siblingNode, true);
+            } else {
+                leftRotate(siblingNode, true);
+            }
+            if(siblingNode.parent == null) {
+                rootReference.set(siblingNode);
+            }
+        }
+        deleteCase3(doubleBlackNode, rootReference);
+    }
+
+    private void deleteCase3(Node doubleBlackNode, AtomicReference<Node> rootReference) {
+
+        Node siblingNode = findSiblingNode(doubleBlackNode).get();
+
+        if(doubleBlackNode.parent.color == Color.BLACK && siblingNode.color == Color.BLACK && siblingNode.left.color == Color.BLACK
+                && siblingNode.right.color == Color.BLACK) {
+            siblingNode.color = Color.RED;
+            deleteCase1(doubleBlackNode.parent, rootReference);
+        } else {
+            deleteCase4(doubleBlackNode, rootReference);
+        }
+    }
+
+    private void deleteCase4(Node doubleBlackNode, AtomicReference<Node> rootReference) {
+        Node siblingNode = findSiblingNode(doubleBlackNode).get();
+        if(doubleBlackNode.parent.color == Color.RED && siblingNode.color == Color.BLACK && siblingNode.left.color == Color.BLACK
+        && siblingNode.right.color == Color.BLACK) {
+            siblingNode.color = Color.RED;
+            doubleBlackNode.parent.color = Color.BLACK;
+        } else {
+            deleteCase5(doubleBlackNode, rootReference);
+        }
+    }
+
+    private void deleteCase5(Node doubleBlackNode, AtomicReference<Node> rootReference) {
+        Node siblingNode = findSiblingNode(doubleBlackNode).get();
+        if(siblingNode.color == Color.BLACK) {
+            if (isLeftChild(doubleBlackNode) && siblingNode.right.color == Color.BLACK && siblingNode.left.color == Color.RED) {
+                rightRotate(siblingNode.left, true);
+            } else if (!isLeftChild(doubleBlackNode) && siblingNode.left.color == Color.BLACK && siblingNode.right.color == Color.RED) {
+                leftRotate(siblingNode.right, true);
+            }
+        }
+        deleteCase6(doubleBlackNode, rootReference);
+    }
+
+    private void deleteCase6(Node doubleBlackNode, AtomicReference<Node> rootReference) {
+        Node siblingNode = findSiblingNode(doubleBlackNode).get();
+        siblingNode.color = siblingNode.parent.color;
+        siblingNode.parent.color = Color.BLACK;
+        if(isLeftChild(doubleBlackNode)) {
+            siblingNode.right.color = Color.BLACK;
+            leftRotate(siblingNode, false);
+        } else {
+            siblingNode.left.color = Color.BLACK;
+            rightRotate(siblingNode, false);
+        }
+        if(siblingNode.parent == null) {
+            rootReference.set(siblingNode);
+        }
+    }
+
+    private void replaceNode(Node root, Node child, AtomicReference<Node> rootReference) {
+        child.parent = root.parent;
+        if(root.parent == null) {
+            rootReference.set(child);
+        }
+        else {
+            if(isLeftChild(root)) {
+                root.parent.left = child;
+            } else {
+                root.parent.right = child;
+            }
+        }
+    }
+
     private void printRedBlackTree(Node root, int space) {
-        if(root == null) {
+        if(root == null || root.isNullLeaf) {
             return;
         }
         printRedBlackTree(root.right, space + 5);
@@ -235,6 +390,50 @@ public class RedBlackTree {
         printRedBlackTree(root.left, space + 5);
     }
 
+    public boolean validateRedBlackTree(Node root) {
+
+        if(root == null) {
+            return true;
+        }
+        //check if root is black
+        if(root.color != Color.BLACK) {
+            System.out.print("Root is not black");
+            return false;
+        }
+        AtomicInteger blackCount = new AtomicInteger(0);
+        //make sure black count is same on all path and there is no red red relationship
+        return checkBlackNodesCount(root, blackCount, 0) && noRedRedParentChild(root, Color.BLACK);
+    }
+
+    private boolean noRedRedParentChild(Node root, Color parentColor) {
+        if(root == null) {
+            return true;
+        }
+        if(root.color == Color.RED && parentColor == Color.RED) {
+            return false;
+        }
+
+        return noRedRedParentChild(root.left, root.color) && noRedRedParentChild(root.right, root.color);
+    }
+
+    private boolean checkBlackNodesCount(Node root, AtomicInteger blackCount, int currentCount) {
+
+        if(root.color == Color.BLACK) {
+            currentCount++;
+        }
+
+        if(root.left == null && root.right == null) {
+            if(blackCount.get() == 0) {
+                blackCount.set(currentCount);
+                return true;
+            } else {
+                return currentCount == blackCount.get();
+            }
+        }
+        return checkBlackNodesCount(root.left, blackCount, currentCount) && checkBlackNodesCount(root.right, blackCount, currentCount);
+    }
+
+
     public void printRedBlackTree(Node root) {
         printRedBlackTree(root, 0);
     }
@@ -242,7 +441,7 @@ public class RedBlackTree {
     public static void main(String args[]) {
         Node root = null;
         RedBlackTree redBlackTree = new RedBlackTree();
-      /*  root = redBlackTree.insert(root, 10);
+        root = redBlackTree.insert(root, 10);
         root = redBlackTree.insert(root, 20);
         root = redBlackTree.insert(root, 0);
         root = redBlackTree.insert(root, 25);
@@ -254,7 +453,7 @@ public class RedBlackTree {
         root = redBlackTree.insert(root, 23);
         root = redBlackTree.insert(root, 24);
         root = redBlackTree.insert(root, 5);
-        root = redBlackTree.insert(root, 4);*/
+        root = redBlackTree.insert(root, 4);
         root = redBlackTree.insert(root, 10);
         root = redBlackTree.insert(root, 20);
         root = redBlackTree.insert(root, -10);
@@ -265,5 +464,6 @@ public class RedBlackTree {
         root = redBlackTree.insert(root, 60);
 
         redBlackTree.printRedBlackTree(root);
+        System.out.println(redBlackTree.validateRedBlackTree(root));
     }
 }
