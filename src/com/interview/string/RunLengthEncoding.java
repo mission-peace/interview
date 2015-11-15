bf5df1d067b11dc180235823b113a64ea05fedb2
Deepak Agrawal <bits.deepak9@yahoo.co.in>
2015-11-14 22:46:51 -0800

    Simplifying RunLengthEncoding.java

--- src/com/interview/string/RunLengthEncoding.java
+++ src/com/interview/string/RunLengthEncoding.java
@@ -1,54 +1,18 @@
 package com.interview.string;
 
 public class RunLengthEncoding {
-
-    private int updateCounter(char[] result, int current, int counter){
-    
-        int tempCounter = counter;
-        int len = 1;
-        while(tempCounter > 0){
-            len = len*10;
-            tempCounter /= 10;
-        }
-        len = len/10;
-        
-        while(counter > 0){
-            result[current++] = (char)((counter/len) + '0');
-            counter = counter%len;
-            len /= 10;
-        }
-        return current;
-    }
-    
-    public int encoding(char[] str,char[] result){
-
-        char pickedChar = str[0];
-        int current =0;
-        int counter =1;
-        for(int i=1; i < str.length; i++){
-            if(str[i] == pickedChar){
-                counter++;
+    public static void main(String[] args) {
+        String input = "AAAAAAAAAAAAABBCDDEEEE";
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            int count = 1;
+            while (i+1 < input.length() && input.charAt(i) == input.charAt(i+1)) {
+                count++;
+                i++;
             }
-            else{
-                result[current++] = pickedChar;
-                current = updateCounter(result, current, counter);
-                pickedChar = str[i];
-                counter =1;
-            }
+            result.append(input.charAt(i));
+            result.append(count);
         }
-        result[current++] = pickedChar;
-        current = updateCounter(result, current, counter);
-        return current;
-    }
-    
-    public static void main(String args[]){
-        
-        String str = "AAAAAAAAAAAAABBCDDEEEEE";
-        char result[] = new char[str.length()*2];
-        RunLengthEncoding rle = new RunLengthEncoding();
-        int current = rle.encoding(str.toCharArray(),result);
-        for(int i=0; i < current; i++){
-            System.out.print(result[i]);
-        }
+        System.out.println(result.toString());
     }
 }
