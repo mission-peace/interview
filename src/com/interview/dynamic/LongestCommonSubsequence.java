0ea4eea6d9e5a9c544124f6f9dcd36ac7a8cec7e
Deepak Agrawal <bits.deepak9@yahoo.co.in>
2015-11-14 23:02:11 -0800

    Update LongestCommonSubsequence
    I think we need to execute temp[i][j] = Math.max(temp[i][j-1],temp[i-1][j]); conditionally not always.

--- src/com/interview/dynamic/LongestCommonSubsequence.java
+++ src/com/interview/dynamic/LongestCommonSubsequence.java
@@ -24,12 +24,15 @@ public class LongestCommonSubsequence {
         int max = 0;
         for(int i=1; i < temp.length; i++){
             for(int j=1; j < temp[i].length; j++){
-                temp[i][j] = Math.max(temp[i][j-1],temp[i-1][j]);
-                if(str1[i-1] == str2[j-1]){
-                    temp[i][j] = temp[i-1][j-1] + 1;
-                    if(temp[i][j] > max){
-                        max = temp[i][j];
-                    }
+                if(str1[i-1] == str2[j-1]) {
+                    temp[i][j] = temp[i - 1][j - 1] + 1;
+                }
+                else
+                {
+                    temp[i][j] = Math.max(temp[i][j-1],temp[i-1][j]);
+                }
+                if(temp[i][j] > max){
+                    max = temp[i][j];
                 }
             }
         }
