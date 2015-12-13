706a80a00b8329c80eb39f351e3bdb4e7c5042dc
Tushar Roy <tushar_v_roy@apple.com>
2015-12-12 19:42:25 -0800

    python code for rabin karp

---
+++ python/string/rabinkarp.py
@@ -0,0 +1,60 @@
+#Rabin Karp algorithm
+# Java code https://github.com/mission-peace/interview/blob/master/src/com/interview/string/RabinKarpSearch.java
+
+prime = 11
+def pattern_matching(text, pattern):
+    m = len(pattern)
+    n = len(text)
+    pattern_hash = create_hash(pattern, m - 1)
+    text_hash = create_hash(text, m - 1)
+
+    for i in range(1, n - m + 1):
+        if pattern_hash == text_hash:
+            if check_equal(text[i-1:i+m-2], pattern[0:m-1]) is True:
+                return i - 1;
+        text_hash = recalculate_hash(text,i-1,i+m-1,text_hash)
+
+    if pattern_hash == text_hash: 
+        if check_equal(text[n-m:n-1],pattern[0:m-1]) is True:
+            return n - m
+    return -1;
+    
+    
+            
+
+def check_equal(str1, str2):
+    if len(str1) != len(str2):
+        return False;
+    i = 0
+    j = 0
+    for i, j in zip(str1, str2):
+        if i != j:
+            return False;
+    return True
+    
+def create_hash(input, end):
+    hash = 0
+    for i in range(end + 1):
+        hash = hash + ord(input[i])*pow(prime, i)
+    return hash
+
+def recalculate_hash(input, old_index, new_index, hash):
+    size = new_index - old_index
+    hash -= ord(input[old_index])
+    hash = hash/prime
+    hash += ord(input[new_index])*pow(prime, size - 1)
+    return hash;
+
+
+index = pattern_matching("TusharRoy", "sharRoy")
+print("Index ", index)
+index = pattern_matching("TusharRoy", "Roy")
+print("Index ", index)
+index = pattern_matching("TusharRoy", "shas")
+print("Index ", index)
+index = pattern_matching("TusharRoy", "usha")
+print("Index ", index)
+index = pattern_matching("TusharRoy", "Tus")
+print("Index ", index)
+index = pattern_matching("TusharRoy", "Roa")
+print("Index ", index)
