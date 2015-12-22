a82f7f3007d206f064ad9326f06fee19ddfb4c40
upa8 <pratikupacharya@gmail.com>
2015-12-22 23:38:02 +0530

    Iterative BST checking method added

--- src/com/interview/tree/IsBST.java
+++ src/com/interview/tree/IsBST.java
@@ -1,5 +1,8 @@
 package com.interview.tree;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * Date 04/11/2015
  * @author tusroy
@@ -23,31 +26,72 @@ package com.interview.tree;
  */
 public class IsBST {
 
-    public boolean isBST(Node root){
-        return isBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
-    }
-    
-    private boolean isBST(Node root, int min, int max){
-        if(root == null){
-            return true;
-        }
-        if(root.data <= min || root.data > max){
-            return false;
-        }
-        return isBST(root.left, min, root.data) && isBST(root.right, root.data, max);
-    }
-    
-    public static void main(String args[]){
-        BinaryTree bt = new BinaryTree();
-        Node root = null;
-        root = bt.addNode(10, root);
-        root = bt.addNode(15, root);
-        root = bt.addNode(-10, root);
-        root = bt.addNode(17, root);
-        root = bt.addNode(20, root);
-        root = bt.addNode(0, root);
-        
-        IsBST isBST = new IsBST();
-        assert isBST.isBST(root);
-    }
+	public boolean isBST(Node root) {
+		return this.isBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+	}
+
+	private boolean isBST(Node root, int min, int max) {
+		if (root == null) {
+			return true;
+		}
+		if ((root.data <= min) || (root.data > max)) {
+			return false;
+		}
+		return this.isBST(root.left, min, root.data)
+		        && this.isBST(root.right, root.data, max);
+	}
+
+	public boolean isBSTEfficient(Node root) {
+		if (root == null) {
+			return true;
+		}
+		Deque<Node> stack = new LinkedList<Node>();
+		Node node = root;
+		int prev = node.data;
+		int current = 0;
+		while ( true ) {
+			if (node != null) {
+				stack.addFirst(node);
+				node = node.left;
+			} else {
+				if (stack.isEmpty()) {
+					break;
+				}
+				node = stack.pollFirst();
+				current = node.data;
+				prev = node.data;
+				node = node.right;
+				if (current > prev) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	public static void main(String args[]) {
+		BinaryTree bt = new BinaryTree();
+		Node root = null;
+		root = bt.addNode(10, root);
+		root = bt.addNode(15, root);
+		root = bt.addNode(-10, root);
+		root = bt.addNode(17, root);
+		root = bt.addNode(20, root);
+		root = bt.addNode(0, root);
+
+		IsBST isBST = new IsBST();
+
+		if (isBST.isBST(root)) {
+			System.out.println("BST");
+		} else {
+			System.out.println("No-Bst");
+		}
+
+		if (isBST.isBSTEfficient(root)) {
+			System.out.println("BST");
+		} else {
+			System.out.println("No-Bst");
+		}
+	}
 }
