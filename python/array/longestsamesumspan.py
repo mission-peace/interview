f7bb8fec53763bf5b39181ce5ccc45461fe43703
Tushar Roy <tushar_v_roy@apple.com>
2015-12-29 11:43:22 -0600

    longest common span of same sum

---
+++ python/array/longestsamesumspan.py
@@ -0,0 +1,27 @@
+# http://www.geeksforgeeks.org/longest-span-sum-two-binary-arrays/
+
+def longest_span(input1, input2):
+    if len(input1) != len(input2):
+        raise ValueError;
+
+    diff = {}
+    prefix1 = 0
+    prefix2 = 0
+    max_span = 0
+    for i in range(len(input1)):
+        prefix1 += input1[i]
+        prefix2 += input2[i]
+        curr_diff = prefix1 - prefix2
+        if curr_diff in diff:
+            max_span = max(max_span, i - diff[curr_diff])
+        else:
+            diff[curr_diff] = i
+    return max_span
+
+if __name__ == '__main__':
+    input1 = [0, 1, 0, 1, 1, 1, 1]
+    input2 = [1, 1, 1, 1, 1, 0, 1]
+
+    print(longest_span(input1, input2))
+    
+    
