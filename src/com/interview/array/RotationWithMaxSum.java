97405ece2b5f14b1e687c425590e5c3ff73f1290
Tushar Roy <tushar_v_roy@apple.com>
2015-12-30 10:55:34 -0600

    add time/space complexity for rotation with max sum

--- src/com/interview/array/RotationWithMaxSum.java
+++ src/com/interview/array/RotationWithMaxSum.java
@@ -6,6 +6,9 @@ package com.interview.array;
  *
  * Given an input array find which rotation will give max sum of i * arr[i]
  *
+ * Time complexity - O(n)
+ * Space complexity - O(1)
+ *
  * http://www.geeksforgeeks.org/find-maximum-value-of-sum-iarri-with-only-rotations-on-given-array-allowed/
  */
 public class RotationWithMaxSum {
