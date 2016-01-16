22354cdfdab1aca6276e5fa6c50a7758d3701666
Tushar Roy <tushar_v_roy@apple.com>
2016-01-15 19:44:54 -0800

    update cycle in directed graph program

--- python/graph/cycledirectedgraph.py
+++ python/graph/cycledirectedgraph.py
@@ -3,67 +3,47 @@
 
 from graph import *
 
-GRAY = "gray"
-BLACK = "black"
-WHITE = "white"
-
 def has_cycle(graph):
-    visited = set()
-    current_stack = set()
-    for vertex in graph.all_vertex.values():
-        if vertex not in visited:
-            result = has_cycle_util(vertex, visited, current_stack)
-            if result:
-                return True
-    return False
+    white = set()
+    gray = set()
+    black = set()
 
-def has_cycle_util(vertex, visited, current_stack):
-    visited.add(vertex)
-    current_stack.add(vertex)
-    for adjacent in vertex.adjacent_vertices:
-        if adjacent in current_stack:
+    for vertex in graph.all_vertex.values():
+        white.add(vertex)
+
+    while len(white) > 0:
+        current = next(iter(white))       
+        if dfs(current, white, gray, black) == True:
             return True
-        else:
-            result = has_cycle_util(adjacent, visited, current_stack)
-            if result:
-                return True
 
-    current_stack.remove(vertex)
-    return False
+    return False        
 
-def has_cycle_using_back_edge(graph):
-    visited = {}
-    for vertex in graph.all_vertex.values():
-        visited[vertex] = WHITE
-
-    for vertex in graph.all_vertex.values():
-        if visited[vertex] == WHITE:
-            result = has_cycle_using_back_edge_util(vertex, visited)
-            if result:
-                return True
-    return False
-
-def has_cycle_using_back_edge_util(vertex, visited):
-    if visited[vertex] == GRAY:
-        return True
-    visited[vertex] = GRAY
-    for adjacent in vertex.adjacent_vertices:
-        result = has_cycle_using_back_edge_util(adjacent, visited)
-        if result:
+def dfs(current, white, gray, black):
+    move_vertex(current, white, gray)
+    for neighbor in current.adjacent_vertices:
+        if neighbor in black:
+            continue
+        if neighbor in gray:
             return True
-    visited[vertex] = BLACK
-    return False
+        if dfs(neighbor, white, gray, black) == True:
+            return True
         
+    move_vertex(current, gray, black)
+    return False
 
+def move_vertex(vertex, source_set, destination_set):
+    source_set.remove(vertex)
+    destination_set.add(vertex)
+        
 if __name__ == '__main__':
     graph = Graph(True)
     graph.add_edge(1,2)
     graph.add_edge(1,3)
-    graph.add_edge(3,4)
+    graph.add_edge(2,3)
+    graph.add_edge(4,1)
     graph.add_edge(4,5)
-    graph.add_edge(2,5)
-    graph.add_edge(5,3)
+    graph.add_edge(5,6)
+    graph.add_edge(6,4)
 
-    result1 = has_cycle(graph)
-    result2 = has_cycle_using_back_edge(graph)
-    print(str(result1) + " " + str(result2))
+    print(has_cycle(graph));
+     
