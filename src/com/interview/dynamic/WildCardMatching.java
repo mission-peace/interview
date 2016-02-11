932a9594e7da44cacd505956d8873bb80ec8ada0
Tushar Roy <tushar_v_roy@apple.com>
2016-02-11 14:08:17 -0800

    recursive version of wild card matching

--- src/com/interview/dynamic/WildCardMatching.java
+++ src/com/interview/dynamic/WildCardMatching.java
@@ -48,4 +48,39 @@ public class WildCardMatching {
 
         return T[text.length][writeIndex];
     }
+
+
+    /**
+     * Recursive and slow version of wild card matching.
+     */
+    public boolean isMatchRecursive(String s, String p) {
+        return isMatchRecursiveUtil(s.toCharArray(), p.toCharArray(), 0, 0);
+    }
+
+    private boolean isMatchRecursiveUtil(char[] text, char[] pattern, int pos1, int pos2) {
+        if (pos2 == pattern.length) {
+            return text.length == pos1;
+        }
+
+        if (pattern[pos2] != '*') {
+            if (pos1 < text.length && (text[pos1] == pattern[pos2]) || pattern[pos2] == '?') {
+                return isMatchRecursiveUtil(text, pattern, pos1 + 1, pos2 + 1);
+            } else {
+                return false;
+            }
+        } else {
+            //if we have a***b then skip to the last *
+            while (pos2 < pattern.length - 1 && pattern[pos2 + 1] == '*') {
+                pos2++;
+            }
+            pos1--;
+            while (pos1 < text.length) {
+                if (isMatchRecursiveUtil(text, pattern, pos1 + 1, pos2 + 1)) {
+                    return true;
+                }
+                pos1++;
+            }
+            return false;
+        }
+    }
 }
