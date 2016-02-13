882b54ee088c9b83371ac729a43c58c89e52eee6
Tushar Roy <tushar_v_roy@apple.com>
2016-02-12 23:39:15 -0800

    update wildcard

--- src/com/interview/dynamic/WildCardMatching.java
+++ src/com/interview/dynamic/WildCardMatching.java
@@ -11,9 +11,11 @@ package com.interview.dynamic;
  */
 public class WildCardMatching {
     public boolean isMatch(String s, String p) {
-        char[] text = s.toCharArray();
+        char[] str = s.toCharArray();
         char[] pattern = p.toCharArray();
 
+        //replace multiple * with one *
+        //e.g a**b***c --> a*b*c
         int writeIndex = 0;
         boolean isFirst = true;
         for ( int i = 0 ; i < pattern.length; i++) {
@@ -28,7 +30,7 @@ public class WildCardMatching {
             }
         }
 
-        boolean T[][] = new boolean[text.length + 1][writeIndex + 1];
+        boolean T[][] = new boolean[str.length + 1][writeIndex + 1];
 
         if (writeIndex > 0 && pattern[0] == '*') {
             T[0][1] = true;
@@ -38,7 +40,7 @@ public class WildCardMatching {
 
         for (int i = 1; i < T.length; i++) {
             for (int j = 1; j < T[0].length; j++) {
-                if (pattern[j-1] == '?' || text[i-1] == pattern[j-1]) {
+                if (pattern[j-1] == '?' || str[i-1] == pattern[j-1]) {
                     T[i][j] = T[i-1][j-1];
                 } else if (pattern[j-1] == '*'){
                     T[i][j] = T[i-1][j] || T[i][j-1];
@@ -46,7 +48,7 @@ public class WildCardMatching {
             }
         }
 
-        return T[text.length][writeIndex];
+        return T[str.length][writeIndex];
     }
 
 
@@ -82,5 +84,10 @@ public class WildCardMatching {
             }
             return false;
         }
+    }
+
+    public static void main(String args[]) {
+        WildCardMatching wcm = new WildCardMatching();
+        System.out.println(wcm.isMatch("xbylmz", "x?y*z"));
     }
 }
