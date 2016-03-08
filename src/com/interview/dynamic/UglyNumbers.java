e06f9f9e4ee2bfee83cbc10e67709ae052c35294
Tushar Roy <tushar_v_roy@apple.com>
2016-03-08 09:20:55 -0800

    update ugly number

--- src/com/interview/dynamic/UglyNumbers.java
+++ src/com/interview/dynamic/UglyNumbers.java
@@ -1,9 +1,56 @@
 package com.interview.dynamic;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
 /**
+ * Date 03/08/2016
+ * @author Tushar Roy
+ *
+ * Find nth ugly number.
+ *
+ * https://leetcode.com/problems/super-ugly-number/
  * http://www.geeksforgeeks.org/ugly-numbers/
  */
 public class UglyNumbers {
+
+    static class Node implements Comparable<Node> {
+        int inputIndex;
+        int index;
+        int val;
+        Node (int inputIndex, int index, int val) {
+            this.index = index;
+            this.val = val;
+            this.inputIndex = inputIndex;
+        }
+
+        @Override
+        public int compareTo(Node other) {
+            return this.val - other.val;
+        }
+    }
+
+    public int nthSuperUglyNumber1(int n, int[] primes) {
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        for (int i = 0; i < primes.length; i++) {
+            pq.offer(new Node(i, 0, primes[i]));
+        }
+        int[] val = new int[n];
+        val[0] = 1;
+        for (int i = 1; i < n; ) {
+            Node node = pq.poll();
+            if (val[i-1] != node.val) {
+                val[i] = node.val;
+                i++;
+            }
+            node.index = node.index + 1;
+            node.val = primes[node.inputIndex]*val[node.index];
+            pq.offer(node);
+        }
+        return val[n - 1];
+    }
 
     int ugly(int n){
         int arr[] = new int[n];
@@ -13,7 +60,7 @@ public class UglyNumbers {
         int i3 = 0;
         int i5 = 0;
         while(count < n){
-            int minNumber = min(arr[i2]*2, arr[i3]*3,arr[i5]*5);
+            int minNumber = min(arr[i2] * 2, arr[i3] * 3, arr[i5] * 5);
             if(minNumber == arr[i2]*2){
                 i2++;
             }
@@ -34,10 +81,12 @@ public class UglyNumbers {
         return Math.min(l, c);
     }
     
-    public static void main(String args[]){
+    public static void main(String args[]) {
         UglyNumbers ugly = new UglyNumbers();
         int result = ugly.ugly(150);
         System.out.println(result);
+        int[] primes = {2, 3, 7, 11};
+        System.out.print(ugly.nthSuperUglyNumber1(17, primes));
     }
     
 }
