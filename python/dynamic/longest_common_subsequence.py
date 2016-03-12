e18facdbf3754270d809d73ca033d49dd06e8390
Senthil Kumaran <senthil@uthcode.com>
2016-03-12 10:37:58 -0800

    Code improvement for longest common subsequence problem.

--- python/dynamic/longest_common_subsequence.py
+++ python/dynamic/longest_common_subsequence.py
@@ -40,21 +40,21 @@ def longest_common_subsequence_recursive(sequence1, sequence2):
 
 
 def longest_common_subsequence(sequence1, sequence2):
-    rows = len(sequence1) + 1   # Add 1 to represent 0 valued row for DP
-    cols = len(sequence2) + 1   # Add 1 to represent 0 valued column for DP
+    cols = len(sequence1) + 1   # Add 1 to represent 0 valued column for DP
+    rows = len(sequence2) + 1   # Add 1 to represent 0 valued row for DP
 
-    T = [[0 for _ in range(rows)] for _ in range(cols)]
+    T = [[0 for _ in range(cols)] for _ in range(rows)]
 
-    max_length = float("-inf")
+    max_length = 0
 
-    for index_i in range(1, rows):
-        for index_j in range(1, cols):
-            if sequence1[index_i - 1] == sequence2[index_j - 1]:
-                T[index_j][index_i] = 1 + T[index_j - 1][index_i - 1]
+    for i in range(1, rows):
+        for j in range(1, cols):
+            if sequence2[i - 1] == sequence1[j - 1]:
+                T[i][j] = 1 + T[i - 1][j - 1]
             else:
-                T[index_j][index_i] = max(T[index_j - 1][index_i], T[index_j][index_i - 1])
+                T[i][j] = max(T[i - 1][j], T[i][j - 1])
 
-            max_length = max(max_length, T[index_j][index_i])
+            max_length = max(max_length, T[i][j])
 
     return max_length
 
@@ -64,4 +64,6 @@ if __name__ == '__main__':
     sequence2 = "AEDPHR"
     expected_length = 4
     assert expected_length == longest_common_subsequence_recursive(sequence1, sequence2)
+    assert expected_length == longest_common_subsequence_recursive(sequence2, sequence1)
     assert expected_length == longest_common_subsequence(sequence1, sequence2)
+    assert expected_length == longest_common_subsequence(sequence2, sequence1)
