860562d35d4328415dd5ae1ef2cb0560699605f9
Senthil Kumaran <senthil@uthcode.com>
2016-03-13 01:08:44 -0800

    Edit Distance Problem.

--- python/dynamic/editdistance.py
+++ python/dynamic/editdistance.py
@@ -1,45 +1,67 @@
-#edit distance dynamic programming
-# java code https://github.com/mission-peace/interview/blob/master/src/com/interview/dynamic/EditDistance.java
-def bottom_up_dp(str1, str2):
-    m = len(str1) + 1
-    n = len(str2) + 1
-    T = [[0 for x in range(n)] for x in range(m)]
-    for i in range(n):
-        T[0][i] = i
-        
-    for i in range(m):
-        T[i][0] = i
+"""
+Problem Statement
+=================
 
-    for i in range(1, m):
-        for j in range(1, n):
-            if str1[i-1] == str2[j-1]:
-                T[i][j] = T[i-1][j-1]
-            else:
-                T[i][j] = 1 + min(T[i-1][j-1], T[i-1][j], T[i][j-1])
+Given two strings str1 and str2, find the minimum number of edits (edit one character to another, delete char from str1
+or delete char from str2) to change str1 to str2.
 
-    print_actual_edits(T, str1, str2)            
-    return T[m-1][n-1]
+Video
+-----
+* https://youtu.be/We3YDTzNXEk
 
-def print_actual_edits(T, str1, str2):
+Analysis
+--------
+* Runtime : O(len(str1) * len(str2))
+
+"""
+
+
+def print_edits(T, str1, str2):
     i = len(T) - 1
     j = len(T[0]) - 1
     while True:
         if i == 0 or j == 0:
             break
-        if str1[i-1] == str2[j-1]:
-            i = i-1
-            j = j-1
-        elif T[i][j] == T[i-1][j-1] + 1:
-            print("Edit " + str2[j-1] + " in string2 to " + str1[i-1] + " in string1")
-            i = i - 1
-            j = j - 1
-        elif T[i][j] == T[i-1][j] + 1:
-            print("Delete in string1 " + str1[i-1])
-            i = i - 1
-        elif T[i][j] == T[i][j-1] + 1:
-            print("Delete in string2 " + str2[j-1])
-            j = j - 1
-    
+        if str2[i - 1] == str1[j - 1]:
+            i -= 1
+            j -= 1
+        elif T[i][j] == T[i - 1][j - 1] + 1:
+            print "Edit %s in string1 to %s in string2." % (str1[j - 1], str2[i - 1])
+            i -= 1
+            j -= 1
+        elif T[i][j] == T[i - 1][j] + 1:
+            print "Delete %s in string2." % str2[i - 1]
+            i -= 1
+        elif T[i][j] == T[i][j - 1] + 1:
+            print "Delete %s in string1." % str1[j - 1]
+            j -= 1
+
+
+def min_edit_distance(str1, str2):
+    rows = len(str2) + 1
+    cols = len(str1) + 1
+    T = [[0 for _ in range(cols)] for _ in range(rows)]
+
+    for j in range(cols):
+        T[0][j] = j
+
+    for i in range(rows):
+        T[i][0] = i
+
+    for i in range(1, rows):
+        for j in range(1, cols):
+            if str2[i - 1] == str1[j - 1]:
+                T[i][j] = T[i - 1][j - 1]
+            else:
+                T[i][j] = 1 + min(T[i - 1][j - 1], T[i - 1][j], T[i][j - 1])
+
+    print_edits(T, str1, str2)
+    return T[rows - 1][cols - 1]
+
+
 if __name__ == '__main__':
-    result = bottom_up_dp(list('azced'), list('abcdef'))
-    print(result)
+    str1 = "azced"
+    str2 = "abcdef"
+    expected = 3
+    assert expected == min_edit_distance(str1, str2)
+    assert expected == min_edit_distance(str2, str1)
