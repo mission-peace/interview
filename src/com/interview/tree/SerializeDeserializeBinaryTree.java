16c97580df3623bd075cf8988218f44f9a02b2a5
Tushar Roy <tushar_v_roy@apple.com>
2016-03-13 12:14:03 -0700

    add another method for serialization/deserialization

--- src/com/interview/tree/SerializeDeserializeBinaryTree.java
+++ src/com/interview/tree/SerializeDeserializeBinaryTree.java
@@ -1,5 +1,9 @@
 package com.interview.tree;
 
+import javax.swing.tree.TreeNode;
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * Date 03/12/2016
  * @author Tushar Roy
@@ -14,7 +18,11 @@ package com.interview.tree;
  */
 public class SerializeDeserializeBinaryTree {
 
-    // Encodes a tree to a single string.
+    /**
+     * Serialize Tree using preorder DFS
+     * @param root
+     * @return
+     */
     public String serialize(Node root) {
         StringBuffer buff = new StringBuffer();
         serializeUtil(root, buff);
@@ -39,14 +47,18 @@ public class SerializeDeserializeBinaryTree {
     }
     int index = 0;
 
-    // Decodes your encoded data to tree.
+    /**
+     * Deserialize Tree using preorder DFS
+     * @param data
+     * @return
+     */
     public Node deserialize(String data) {
         String[] input = data.split(",");
         index = 0;
         return deserializeUtil(input);
     }
 
-    public Node deserializeUtil(String input[]) {
+    private Node deserializeUtil(String input[]) {
         if (index == input.length) {
             return null;
         }
@@ -68,6 +80,76 @@ public class SerializeDeserializeBinaryTree {
         }
         return n;
     }
-}
 
+    /**
+     * Serialize tree using level order traversal.
+     */
+    public String serializeLevelOrder(Node root) {
+        if (root == null) {
+            return "";
+        }
+
+        Deque<Node> queue = new LinkedList<>();
+        queue.offerFirst(root);
+        StringBuffer buff = new StringBuffer();
+        while (!queue.isEmpty()) {
+            root = queue.pollFirst();
+            if (root == null) {
+                buff.append("%,");
+            } else {
+                buff.append(root.data).append(",");
+                queue.offer(root.left);
+                queue.offer(root.right);
+            }
+        }
+        for (int i = buff.length() - 1; i >= 0; i--) {
+            if (buff.charAt(i) == '%' || buff.charAt(i) == ',') {
+                buff.deleteCharAt(i);
+            } else {
+                break;
+            }
+        }
+        return buff.toString();
+    }
+
+    /**
+     * Deserialize Tree using level order traversal.
+     */
+    public Node deserializeLevelOrder(String data) {
+        if (data == null || data.length() == 0) {
+            return null;
+        }
+        String[] input = data.split(",");
+        Deque<Node> queue = new LinkedList<>();
+        int index = 0;
+        queue.offerFirst(Node.newNode(Integer.parseInt(input[index])));
+        Node root = queue.peekFirst();
+        index++;
+        while (!queue.isEmpty()) {
+            Node current = queue.pollFirst();
+            if (index < input.length && !input[index].equals("%")) {
+                current.left = Node.newNode(Integer.parseInt(input[index]));
+                queue.offerLast(current.left);
+            }
+            index++;
+            if (index < input.length && !input[index].equals("%")) {
+                current.right = Node.newNode(Integer.parseInt(input[index]));
+                queue.offerLast(current.right);
+            }
+            index++;
+        }
+        return root;
+    }
+
+
+    public static void main(String args[]) {
+        SerializeDeserializeBinaryTree sd = new SerializeDeserializeBinaryTree();
+        Node node = sd.deserialize("10,$,30,15,$,%,20,$,21,16,$,%,18");
+        TreeTraversals tt = new TreeTraversals();
+        tt.inOrder(node);
+        String serializedTree= sd.serializeLevelOrder(node);
+        Node root = sd.deserializeLevelOrder("1,2");
+        tt.inOrder(root);
+    }
+}
 
