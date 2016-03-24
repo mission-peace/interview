3021e1b4f13adadbca90a53f45ca13d90be8acec
Tushar Roy <mission-peace@users.noreply.github.com>
2016-03-24 09:36:02 -0700

    update connect nodes at same level qs

--- src/com/interview/tree/ConnectNodesAtSameLevel.java
+++ src/com/interview/tree/ConnectNodesAtSameLevel.java
@@ -1,84 +1,66 @@
 package com.interview.tree;
 
 /**
- * http://www.geeksforgeeks.org/connect-nodes-at-same-level-with-o1-extra-space/
- * Test cases
- * Empty tree
- * tree with only right childs
- * tree with only left child
- * Tree with short left and long right side
- * Tree with long left and short right side
+ * Date 03/24/2016
+ * @author Tushar Roy
+ *
+ * Populate next pointer for each node of binary tree.
+ *
+ * Time complexity O(n)
+ * Space complexity O(1)
+ *
+ * https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/
  */
 public class ConnectNodesAtSameLevel {
 
-    public void connect(Node root){
-        if(root == null){
+    public void connect(Node root) {
+        if (root == null) {
             return;
         }
-        
-        if(root.left != null && root.right != null){
-            root.left.next = root.right;
-        }
-        
-        if(root.right != null){
-            root.right.next = findRightSibling(root.next);
-        }
-        else if(root.left != null){
-            root.left.next = findRightSibling(root.next);
-        }
-        connect(root.left);
-        connect(root.right);
-    }
-    
-    public void print(Node root){
-        if(root == null){
-            return;
-        }
-        Node temp = root;
-        while(temp != null){
-            System.out.println(temp.data);
-            temp = temp.next;
-        }
-        while(root != null){
-            if(root.left != null){
-                print(root.left);
-                break;
-            }
-            else if(root.right != null){
-                print(root.right);
-                break;
-            }else{
+
+        Node firstNode = root;
+        Node prevNode = null;
+        while (firstNode != null) {
+            root = firstNode;
+            firstNode = null;
+            prevNode = null;
+            while (root != null) {
+                if (root.left != null) {
+                    if (firstNode == null) {
+                        firstNode = root.left;
+                    }
+                    if (prevNode != null) {
+                        prevNode.next = root.left;
+                    }
+                    prevNode = root.left;
+                }
+                if (root.right != null) {
+                    if (firstNode == null) {
+                        firstNode = root.right;
+                    }
+                    if (prevNode != null) {
+                        prevNode.next = root.right;
+                    }
+                    prevNode = root.right;
+                }
                 root = root.next;
             }
         }
     }
-    
-    private Node findRightSibling(Node root){
-        if(root == null){
-            return null;
-        }
-        if(root.left != null){
-            return root.left;
-        }
-        else if(root.right != null){
-            return root.right;
-        }
-        return findRightSibling(root.next);
-    }
-    
+
     public static void main(String args[]){
         BinaryTree bt = new BinaryTree();
-        Node head = null;
-        head = bt.addNode(10, head);
-        head = bt.addNode(15, head);
-        head = bt.addNode(5, head);
-        head = bt.addNode(7, head);
-        head = bt.addNode(19, head);
-        head = bt.addNode(20, head);
-        head = bt.addNode(-1, head);
-        head = bt.addNode(21, head);
+        Node root = null;
+        root = bt.addNode(10, root);
+        root = bt.addNode(15, root);
+        root = bt.addNode(5, root);
+        root = bt.addNode(7, root);
+        root = bt.addNode(19, root);
+        root = bt.addNode(20, root);
+        root = bt.addNode(-1, root);
+        root = bt.addNode(21, root);
         ConnectNodesAtSameLevel cns = new ConnectNodesAtSameLevel();
-        cns.connect(head);
-        cns.print(head);
+
+        cns.connect(root);
     }
 }
