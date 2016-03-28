5741ddeedb35e6b02f7b1a4bf26f458b8a9b365c
Tushar Roy <mission-peace@users.noreply.github.com>
2016-03-28 00:01:00 -0700

    update stock buy sell, read4 questions

--- src/com/interview/array/BuySellStockProfit.java
+++ src/com/interview/array/BuySellStockProfit.java
@@ -1,5 +1,19 @@
 package com.interview.array;
 
+/**
+ * Date 03/04/2016
+ * @author Tushar Roy
+ *
+ * Best time to buy and sell stocks.
+ * 1) Only 1 transaction is allowed
+ * 2) Infinite number transactions are allowed
+ *
+ * Time complexity O(n)
+ * Space complexity O(1)
+ *
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+ */
 public class BuySellStockProfit {
 
     public int oneProfit(int arr[]){
@@ -17,6 +31,9 @@ public class BuySellStockProfit {
     }
     
     public int allTimeProfit(int arr[]){
+        if (arr.length == 0) {
+            return 0;
+        }
         int profit = 0;
         int localMin = arr[0];
         for(int i=1; i < arr.length;i++){
@@ -31,21 +48,6 @@ public class BuySellStockProfit {
         return profit;
     }
     
-    public int allTimeProfit1(int arr[]){
-        int profit = 0;
-        int i =0;
-        while(i < arr.length){
-            while(i+1 < arr.length && arr[i] >= arr[i+1]){
-                i++;
-            }
-            while(i+1 < arr.length && arr[i] < arr[i+1]){
-                profit += arr[i+1] - arr[i];
-                i++;
-            }
-            i++;
-        }
-        return profit;
-    }
     public static void main(String args[]){
         int arr[] = {7,10,15,5,11,2,7,9,3};
         int arr1[] = {6,4,1,3,5,7,3,1,3,4,7,9,2,5,6,0,1,2};
--- src/com/interview/misc/Read4Function.java
+++ src/com/interview/misc/Read4Function.java
@@ -3,102 +3,100 @@ package com.interview.misc;
 import java.util.Arrays;
 
 /**
+ * Date 03/26/2016
+ * @author Tushar Roy
+ * 
  * Given a reader which only reads 4 bytes implement a Reader which can read bytes of give size.
+ *
+ * Reference
+ * Read N Characters Given Read4 II
+ * https://leetcode.com/problems/read-n-characters-given-read4-ii-call-multiple-times/
  */
-public class Read4Function {
 
-    private final Reader reader;
+class Read4 {
+    int read4(char[] buff) {
+        buff[0] = 'a';
+        buff[1] = 'b';
+        buff[2] = 'c';
+        buff[3] = 'd';
+        return 4;
+    }
+}
+
+public class Read4Function extends Read4{
+
+    class Queue {
+        int start;
+        int end;
+        int count;
+        char[] data;
+        int size;
+        Queue(int size) {
+            data = new char[size];
+            this.size = size;
+        }
+
+        boolean isEmpty() {
+            return count == 0;
+        }
+
+        void offer(char b) {
+            data[start] = b;
+            start = (start + 1)%size;
+            count++;
+        }
+
+        char poll() {
+            char d = data[end];
+            end = (end + 1)%size;
+            count--;
+            return d;
+        }
+    }
+
     private final Queue queue;
     public Read4Function() {
-        this.reader = new Reader();
         queue = new Queue(4);
     }
 
-    public byte[] read(int size) {
-        byte[] output = new byte[size];
-        int n = 0;
-        while (!queue.isEmpty() && n < size) {
-            output[n++] = queue.poll();
+    public int read(char[] buf, int n) {
+        int r = 0;
+        while (!queue.isEmpty() && r < n) {
+            buf[r++] = queue.poll();
         }
 
-        if (n == size) {
-            return output;
+        if (r == n) {
+            return r;
         }
+        int index = 0;
+        int readSize = 0;
+        char[] input = null;
+        do {
+            input = new char[4];
+            readSize = read4(input);
+            index = 0;
+            while(r < n && index < readSize) {
+                buf[r++] = input[index++];
+            }
+        } while (readSize == 4 && r < n);
 
-        int remaining = size - n;
-        byte[] b = null;
-        while (remaining > 0) {
-            b = reader.read();
-            copy(b, output, 0, n, remaining > 4 ? 4 : remaining);
-            n += 4;
-            remaining -= 4;
+        while (index < readSize) {
+            queue.offer(input[index++]);
         }
-        for (int i = 4 + remaining; i < 4; i++) {
-            queue.offer(b[i]);
-        }
-
-        return output;
-    }
-
-    private void copy(byte[] src, byte[] destination, int sourceStart, int destinationStart, int size) {
-        int i = sourceStart;
-        int j = destinationStart;
-        while (i < sourceStart + size) {
-            destination[j++] = src[i++];
-        }
+        return r;
     }
 
     public static void main(String args[]) {
         Read4Function rf = new Read4Function();
-        System.out.println(Arrays.toString(rf.read(2)));
-        System.out.println(Arrays.toString(rf.read(1)));
-        System.out.println(Arrays.toString(rf.read(1)));
-        System.out.println(Arrays.toString(rf.read(5)));
-        System.out.println(Arrays.toString(rf.read(2)));
-        System.out.println(Arrays.toString(rf.read(8)));
-        System.out.println(Arrays.toString(rf.read(7)));
-        System.out.println(Arrays.toString(rf.read(17)));
-        System.out.println(Arrays.toString(rf.read(1)));
-    }
-}
-
-class Queue {
-    int start;
-    int end;
-    int count;
-    byte[] data;
-    int size;
-    Queue(int size) {
-        data = new byte[size];
-        this.size = size;
-    }
-
-    boolean isEmpty() {
-        return count == 0;
-    }
-
-    void offer(byte b) {
-        data[start] = b;
-        start = (start + 1)%size;
-        count++;
-    }
-
-    byte poll() {
-        byte d = data[end];
-        end = (end + 1)%size;
-        count--;
-        return d;
-    }
-}
-
-class Reader {
-    int index = 0;
-    byte[] read() {
-        byte[] b = new byte[4];
-        for (int i = index; i < index + 4; i++) {
-            b[i - index] = (byte)i;
-        }
-        index += 4;
-        return b;
+        char[] buff = new char[10];
+        int size = rf.read(buff, 2);
+        System.out.print(size);
+        System.out.println(Arrays.toString(buff));
+        size = rf.read(buff, 1);
+        System.out.print(size);
+        System.out.println(Arrays.toString(buff));
+        size = rf.read(buff, 1);
+        System.out.print(size);
+        System.out.println(Arrays.toString(buff));
     }
 }
