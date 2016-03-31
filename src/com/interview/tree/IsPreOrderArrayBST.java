d6ad9d088f89bb9059ff38f1628cbe05e083deaf
Tushar Roy <mission-peace@users.noreply.github.com>
2016-03-31 09:36:45 -0700

    add constant space method

--- src/com/interview/tree/IsPreOrderArrayBST.java
+++ src/com/interview/tree/IsPreOrderArrayBST.java
@@ -13,12 +13,12 @@ import java.util.LinkedList;
  * It does not work for duplicates
  *
  * Time complexity - O(n)
- * Space complexity - O(n)
+ * https://leetcode.com/problems/verify-preorder-sequence-in-binary-search-tree/
  * Reference - http://www.geeksforgeeks.org/check-if-a-given-array-can-represent-preorder-traversal-of-binary-search-tree/
  */
 public class IsPreOrderArrayBST {
 
-    public boolean isCorrect(int pre[]) {
+    public boolean verifyPreorder(int pre[]) {
         Deque<Integer> deque = new LinkedList<>();
         int minAcceptable = Integer.MIN_VALUE;
         deque.addFirst(pre[0]);
@@ -34,15 +34,42 @@ public class IsPreOrderArrayBST {
         return true;
     }
 
+    public boolean verifyPreorderConstantSpace(int[] preorder) {
+        int start = 0;
+        int min = Integer.MIN_VALUE;
+        for (int i = 1; i < preorder.length; i++) {
+            if (preorder[i] < min) {
+                return false;
+            }
+            if (preorder[i] > preorder[i - 1]) {
+                int index = binarySearch(preorder, start, i - 1, preorder[i]);
+                min = preorder[index];
+                start = i;
+            }
+        }
+        return true;
+    }
+
+    int binarySearch(int[] preorder, int start, int end, int val) {
+        int s = start;
+        int e = end;
+        while (s <= e) {
+            int middle = (s + e)/2;
+            if (preorder[middle] < val && (start == middle || preorder[middle - 1] > val)) {
+                return middle;
+            } else if (preorder[middle] < val) {
+                e = middle - 1;
+            } else {
+                s = middle + 1;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String args[]) {
         IsPreOrderArrayBST isb = new IsPreOrderArrayBST();
-        int pre1[] = {10, -5, -20};
-        System.out.println(isb.isCorrect(pre1));
-        int pre2[] = {10, -5, -20, -10, 7, -6};
-        System.out.println(isb.isCorrect(pre2));
-        int pre3[] = {40, 30, 35, 80, 100};
-        System.out.println(isb.isCorrect(pre3));
-        int pre4[] = {40, 30, 35, 20, 80, 100};
-        System.out.println(isb.isCorrect(pre4));
+        int[] input = {10, 3, -1, 4, 13, 11, 12, 10};
+        System.out.println(isb.verifyPreorder(input));
+        System.out.println(isb.verifyPreorderConstantSpace(input));
     }
 }
