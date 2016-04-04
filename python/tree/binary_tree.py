ea966b2f10df87e0c910117a8bb72d535321cd3a
Senthil <skumaran@gatech.edu>
2016-04-03 19:46:50 -0700

    Morris Traversal (Inorder and Preorder) in Binary Tree.

--- python/tree/binary_tree.py
+++ python/tree/binary_tree.py
@@ -23,3 +23,34 @@ class Node:
         n.size = 1
         n.color = Color.RED
         return n
+
+
+class BinaryTree:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def add_head(data, head):
+        temp_head = head
+        n = Node.newNode(data)
+
+        if head is None:
+            head = n
+            return head
+
+        prev = None
+
+        while head is not None:
+            prev = head
+            if head.data < data:
+                head = head.right
+            else:
+                head = head.left
+
+        if prev.data < data:
+            prev.right = n
+        else:
+            prev.left = n
+
+        return temp_head
