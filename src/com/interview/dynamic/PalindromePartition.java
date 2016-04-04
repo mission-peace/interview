a1d66c45b19ac8db889dca3b6778e91f1192a464
Tushar Roy <mission-peace@users.noreply.github.com>
2016-04-03 23:29:07 -0700

    add one more method in palindrome partitioning

--- src/com/interview/dynamic/PalindromePartition.java
+++ src/com/interview/dynamic/PalindromePartition.java
@@ -1,7 +1,15 @@
 package com.interview.dynamic;
 
+import com.interview.recursion.StringPermutation;
+
+import java.util.*;
+
 /**
+ * Date 04/03/2016
+ * @author Tushar Roy
+ *
  * http://www.geeksforgeeks.org/dynamic-programming-set-17-palindrome-partitioning/
+ * https://leetcode.com/problems/palindrome-partitioning/
  */
 public class PalindromePartition {
 
@@ -38,11 +46,57 @@ public class PalindromePartition {
         }
         return T[0][T.length-1];
     }
-    
-    public static void main(String args[]){
-        String str = "ABCCDCCLMLCCD";
-        PalindromePartition pp = new PalindromePartition();
-        int r = pp.minCuts(str.toCharArray());
-        System.out.print(r);
+
+    /**
+     * Given a string s, partition s such that every substring of the partition is a palindrome.
+     * https://leetcode.com/problems/palindrome-partitioning/
+     */
+    public List<List<String>> partition(String s) {
+        Map<Integer, List<List<String>>> dp = new HashMap<>();
+        List<List<String>> result =  partitionUtil(s, dp, 0);
+        List<List<String>> r = new ArrayList<>();
+        for (List<String> l : result) {
+            r.add(l);
+        }
+        return r;
+    }
+
+    private List<List<String>> partitionUtil(String s, Map<Integer, List<List<String>>> dp, int start) {
+        if (start == s.length()) {
+            List<String> r = new ArrayList<>();
+            return Collections.singletonList(r);
+        }
+
+        if (dp.containsKey(start)) {
+            return dp.get(start);
+        }
+
+        List<List<String>> words = new ArrayList<>();
+        for (int i = start; i < s.length(); i++) {
+            if (!isPalindrome(s, start, i) ) {
+                continue;
+            }
+            String newWord = s.substring(start, i + 1);
+            List<List<String>> result = partitionUtil(s, dp, i + 1);
+            for (List l : result) {
+                List<String> l1 = new ArrayList<>();
+                l1.add(newWord);
+                l1.addAll(l);
+                words.add(l1);
+            }
+        }
+        dp.put(start, words);
+        return words;
+    }
+
+    private  boolean isPalindrome(String str, int r, int t) {
+        while(r < t) {
+            if (str.charAt(r) != str.charAt(t)) {
+                return false;
+            }
+            r++;
+            t--;
+        }
+        return true;
     }
 }
