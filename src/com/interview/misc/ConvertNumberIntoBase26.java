81889dfd31ff51feaf0831e4f5f593bfb1276bc9
Tushar Roy <mission-peace@users.noreply.github.com>
2016-04-09 21:33:41 -0700

    update base 26 qs

--- src/com/interview/misc/ConvertNumberIntoBase26.java
+++ src/com/interview/misc/ConvertNumberIntoBase26.java
@@ -1,32 +1,25 @@
 package com.interview.misc;
 
 /**
- * http://www.careercup.com/question?id=6139456847347712
- * Test case : 0 or negative number
+ * Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+ * https://leetcode.com/problems/excel-sheet-column-title/
  */
 public class ConvertNumberIntoBase26 {
 
-    /**
-     * Here 1..26 is A...Z
-     * 27...52 is AA...AZ. So we first try to divide num by 26 then find ceiling...so 27...52 returns 2. Then we subtract 1 from the number
-     * @param num
-     * @return
-     */
-    public String numberToBase26(int num){
+    public String numberToBase26(int n) {
         StringBuffer buff = new StringBuffer();
-        while(num > 0){
-            char r  = convert(num % 26);
-            buff.append(r);
-            num = (int)(Math.ceil(num*1.0/26));
-            num--;
+        while (n > 0) {
+            n--;
+            buff.append((char)(n % 26 + 'A'));
+            n = n / 26;
         }
         return buff.reverse().toString();
     }
     
-    public int base26ToNumber(String str){
+    public int base26ToNumber(String str) {
         int result = 0;
         int pow = 1;
-        for(int i = str.length() -1; i>=0; i--){
+        for(int i = str.length() - 1; i>=0; i--) {
             char s = str.charAt(i);
             s = (char)(s - 'A'+1);
             result += s*pow;
@@ -34,16 +27,8 @@ public class ConvertNumberIntoBase26 {
         }
         return result;
     }
-    
-    private char convert(int r){
-        if(r == 0){
-            return 'Z';
-        }else{
-            return (char)(r + 'A'-1);
-        }
-    }
-    
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         ConvertNumberIntoBase26 cni = new ConvertNumberIntoBase26();
         System.out.println(cni.numberToBase26(704));
         System.out.println(cni.base26ToNumber("AAB"));
