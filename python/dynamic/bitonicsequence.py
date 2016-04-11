877d6b5c1cd3d919dc47c3f9517d34a152e83cd8
Senthil <skumaran@gatech.edu>
2016-04-11 09:25:36 -0700

    longest bitonic sequence. minor cleanup.

--- python/dynamic/bitonicsequence.py
+++ python/dynamic/bitonicsequence.py
@@ -1,27 +1,47 @@
-#Bitonic sequence Dynamic programming
-# Java code https://github.com/mission-peace/interview/blob/master/src/com/interview/dynamic/BitonicSequence.java
-def bitonic_sequence( input ):
-    lis = [1]*len(input)
-    lds = [1]*len(input)
+"""
+Problem Statement
+=================
 
-    for i in range(1, len(input)):
+Find the length of the longest Bitonic Sequence in a given sequence of numbers. A Bitonic sequence is a sequence of
+numbers which are increasing and then decreasing.
+
+Video
+-----
+* https://youtu.be/TWHytKnOPaQ
+
+Analysis
+--------
+* Runtime O(n)
+
+Reference
+---------
+* http://www.geeksforgeeks.org/dynamic-programming-set-15-longest-bitonic-subsequence/
+"""
+
+
+def longest_bitonic(sequence):
+    length_of_input = len(sequence)
+    increasing_sequence = [1] * length_of_input
+    decreasing_sequence = [1] * length_of_input
+
+    for i in range(1, length_of_input):
         for j in range(0, i):
-            if input[i] > input[j]:
-                lis[i] = max(lis[i], lis[j] + 1)
+            if sequence[i] > sequence[j]:
+                increasing_sequence[i] = max(increasing_sequence[i], increasing_sequence[j] + 1)
 
-                
-    for i in range(len(input)-2,-1,-1):
-        for j in range(len(input)-1,i,-1):
-            if input[i] > input[j]:
-                lds[i] = max(lds[i], lds[j] + 1)
+    for i in range(length_of_input - 2, -1, -1):
+        for j in range(length_of_input - 1, i, -1):
+            if sequence[i] > sequence[j]:
+                decreasing_sequence[i] = max(decreasing_sequence[i], decreasing_sequence[j] + 1)
 
-    maxVal = 0
-    for i in range(len(input)):
-        if lis[i] + lds[i] - 1 > maxVal:
-            maxVal = lis[i] + lds[i] -1;
+    max_value = 0
+    for i in range(len(sequence)):
+        if increasing_sequence[i] + decreasing_sequence[i] - 1 > max_value:
+            max_value = increasing_sequence[i] + decreasing_sequence[i] - 1
 
-    return maxVal;           
+    return max_value
 
-maxVal= bitonic_sequence([1,4,3,7,2,1,8,11,13,0])
 
-print("Max bitonic sequence", maxVal) 
+if __name__ == '__main__':
+    max_value = longest_bitonic([1, 4, 3, 7, 2, 1, 8, 11, 13, 0])
+    assert 7 == max_value  # 1, 4, 7, 8, 11, 13, 0
