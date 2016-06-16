bfe285493869a3f3e505509a1e978706a3aaafec
Tushar Roy <tushar_v_roy@apple.com>
2016-06-15 21:44:04 -0700

    update immutable2D rectangular sum solution

--- src/com/interview/dynamic/Immutable2DSumRangeQuery.java
+++ src/com/interview/dynamic/Immutable2DSumRangeQuery.java
@@ -4,7 +4,7 @@ package com.interview.dynamic;
  * Date 03/11/2016
  * @author Tushar Roy
  *
- * Given a 2D array find the sum in given range.
+ * Given a 2D array find the sum in given range defining a rectangle.
  *
  * Time complexity construction O(n*m)
  * Time complexity of query O(1)
@@ -15,6 +15,7 @@ package com.interview.dynamic;
  */
 public class Immutable2DSumRangeQuery {
     private int[][] T;
+
     public Immutable2DSumRangeQuery(int[][] matrix) {
         int row = 0;
         int col = 0;
@@ -28,15 +29,7 @@ public class Immutable2DSumRangeQuery {
                 T[i][j] = T[i - 1][j] + T[i][j - 1] + matrix[i - 1][j - 1] - T[i - 1][j - 1];
             }
         }
-
-        for (int i = 1; i < T.length; i++) {
-            for (int j = 1; j < T[0].length; j++) {
-                System.out.print(T[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-    }
+   }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
         row1++;
@@ -52,7 +45,9 @@ public class Immutable2DSumRangeQuery {
                         {1, 2, 0, 1, 5},
                         {4, 1, 0, 1, 7},
                         {1, 0, 3, 0, 5}};
-        Immutable2DSumRangeQuery isr = new Immutable2DSumRangeQuery(input);
+
+        int[][] input1 = {{2,0,-3,4}, {6, 3, 2, -1}, {5, 4, 7, 3}, {2, -6, 8, 1}};
+        Immutable2DSumRangeQuery isr = new Immutable2DSumRangeQuery(input1);
         System.out.println(isr.sumRegion(1, 1, 2, 2));
     }
 }
