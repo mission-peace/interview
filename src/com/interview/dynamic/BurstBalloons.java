02daee496633e7b7283e32d73e2da5701ee7da2e
Tushar Roy <tushar_v_roy@apple.com>
2016-07-05 23:33:06 -0700

    Burst balloon DP

--- src/com/interview/dynamic/BurstBalloons.java
+++ src/com/interview/dynamic/BurstBalloons.java
@@ -4,42 +4,65 @@ package com.interview.dynamic;
  * Date 03/02/2016
  * @author Tushar Roy
  *
+ * Given n balloons, indexed from 0 to n-1. Each balloon is painted with a number on it represented
+ * by array nums. You are asked to burst all the balloons. If the you burst balloon i you will
+ * get nums[left] * nums[i] * nums[right] coins. Here left and right are adjacent indices of i. After the burst,
+ * the left and right then becomes adjacent.
+ * Find the maximum coins you can collect by bursting the balloons wisely.
+ *
  * Time complexity O(n^3)
  * Space complexity O(n^2)
+ *
+ * Reference
  * https://leetcode.com/problems/burst-balloons/
  */
 public class BurstBalloons {
-    public int maxCoins(int[] nums) {
+
+    /**
+     * Dynamic programming solution.
+     */
+    public int maxCoinsBottomUpDp(int[] nums) {
 
         int T[][] = new int[nums.length][nums.length];
 
-        for (int l = 1; l <= nums.length; l++) {
-            for (int i = 0; i <= nums.length - l; i++) {
-                int j = i + l - 1;
+        for (int len = 1; len <= nums.length; len++) {
+            for (int i = 0; i <= nums.length - len; i++) {
+                int j = i + len - 1;
                 for (int k = i; k <= j; k++) {
-                    int left = 1;
-                    int right = 1;
+                    //leftValue/rightValue is initially 1. If there is element on
+                    // left/right of k then left/right value will take that value.
+                    int leftValue = 1;
+                    int rightValue = 1;
+                    if (i != 0) {
+                        leftValue = nums[i-1];
+                    }
+                    if (j != nums.length -1) {
+                        rightValue = nums[j+1];
+                    }
+
+                    //before is initially 0. If k is i then before will
+                    //stay 0 otherwise it gets value T[i][k-1]
+                    //after is similarly 0 initially. if k is j then after will
+                    //stay 0 other will get value T[k+1][j]
                     int before = 0;
+                    int after = 0;
                     if (i != k) {
                         before = T[i][k-1];
                     }
-                    int after = 0;
                     if (j != k) {
                         after = T[k+1][j];
                     }
-                    if (i != 0) {
-                        left = nums[i-1];
-                    }
-                    if (j != nums.length -1) {
-                        right = nums[j+1];
-                    }
-                    T[i][j] = Math.max(left*nums[k]*right + before + after, T[i][j]);
+                    T[i][j] = Math.max(leftValue * nums[k] * rightValue + before + after,
+                            T[i][j]);
                 }
             }
         }
         return T[0][nums.length - 1];
     }
 
+    /**
+     * Recursive solution.
+     */
     public int maxCoinsRec(int nums[]) {
         int[] nums1 = new int[nums.length + 2];
         nums1[0] = 1;
@@ -81,7 +104,7 @@ public class BurstBalloons {
 
     public static void main(String args[]) {
         BurstBalloons bb = new BurstBalloons();
-        int input[] = {3, 1, 5, 8};
-        System.out.print(bb.maxCoins(input));
+        int input[] = {2, 4, 3, 5};
+        System.out.print(bb.maxCoinsBottomUpDp(input));
     }
 }
