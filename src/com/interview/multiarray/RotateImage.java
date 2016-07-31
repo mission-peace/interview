3250d2eb356e8f784f8c47e469703177d4e72e0f
Tushar Roy <tushar_v_roy@apple.com>
2016-07-31 15:39:36 -0700

    Remove extra lines

--- src/com/interview/multiarray/RotateImage.java
+++ src/com/interview/multiarray/RotateImage.java
@@ -15,7 +15,6 @@ public class RotateImage {
 		int length = matrix.length-1;
 		int j=0;
 		while(j < matrix.length/2){
-		
 			for(int i=j; i < length-j; i++){
 				int temp = matrix[j][i];
 				matrix[j][i] = matrix[length-i][j];
@@ -26,7 +25,7 @@ public class RotateImage {
 			j++;
 		}
 	}
-	
+
 	private void print(int arr[][]){
 		for(int i=0; i < arr.length; i++){
 			for(int j=0; j < arr.length; j++){
@@ -35,9 +34,9 @@ public class RotateImage {
 			System.out.print("\n");
 		}
 	}
-	
+
 	public static void main(String args[]){
-		
+
 		int matrix[][] = {{1,2,3,4,20},{5,6,7,8,30},{9,10,11,12,40},{13,14,15,16,50},{21,22,23,24,25}};
 		RotateImage ti = new RotateImage();
 		ti.rotate(matrix);
