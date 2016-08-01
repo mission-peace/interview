fd931518adedad06c93f523b0fa5e7811bbe0131
Tushar Roy <tushar_v_roy@apple.com>
2016-07-31 18:38:08 -0700

    Minimum number of jumps to reach end

--- src/com/interview/array/JumpGame.java
+++ src/com/interview/array/JumpGame.java
@@ -12,6 +12,15 @@ package com.interview.array;
  * Space complexity O(1)
  *
  * https://leetcode.com/problems/jump-game/
+ *
+ *  Given an array of non-negative integers, you are initially positioned at the first index of the array.
+ * Each element in the array represents your maximum jump length at that position.
+ * Your goal is to reach the last index in the minimum number of jumps.
+ *
+ * Time complexity O(n)
+ * Space complexity O(1)
+ *
+ * https://leetcode.com/problems/jump-game-ii/
  */
 public class JumpGame {
 
@@ -24,9 +33,25 @@ public class JumpGame {
         return i == nums.length;
     }
 
+    public int jump(int[] nums) {
+        int current = 0;
+        int max = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            max = Math.max(max, i + nums[i]);
+            if (current == i) {
+                count++;
+                current = max;
+            }
+        }
+        return count;
+    }
+
     public static void main(String args[]) {
         JumpGame jumpGame = new JumpGame();
-        int[] nums = {3, 1};
-        System.out.print(jumpGame.canJump(nums));
+        int[] nums = {3, 2, 3, 0, 2, 1};
+        System.out.println(jumpGame.jump(nums));
+        int[] nums1 = {3, 0, 2, 0, 0, 1};
+        System.out.println(jumpGame.canJump(nums1));
     }
 }
