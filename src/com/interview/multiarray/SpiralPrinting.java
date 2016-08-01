a1389e758f127b66a38eea161817d01609c78bfe
Tushar Roy <tushar_v_roy@apple.com>
2016-07-31 17:38:44 -0700

    fix spiral printing logic

--- src/com/interview/multiarray/SpiralPrinting.java
+++ src/com/interview/multiarray/SpiralPrinting.java
@@ -1,8 +1,12 @@
 package com.interview.multiarray;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Date 03/15/2015 
- * @author tusroy
+ * @author Tushar Roy
  * 
  * Given a 2D matrix(square or rectangular) print it in spiral way.
  * e.g 1 2 3
@@ -15,54 +19,56 @@ package com.interview.multiarray;
  * row or column and keep incrementing and decrementing the bounds. As soon as up meets down
  * or left meets right we are done.
  * 
- * Test cases
- * Null matrix
- * Empty matrix
- * square matrix
- * rectangular matrix
- *
  * Reference
+ * https://leetcode.com/problems/spiral-matrix/
  * http://stackoverflow.com/questions/726756/print-two-dimensional-array-in-spiral-order
  * http://www.geeksforgeeks.org/print-a-given-matrix-in-spiral-form/
  */
 public class SpiralPrinting {
 
-    public void print(int matrix[][]) {
+    public List<Integer> spiralOrder(int[][] matrix) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-            return;
+            return Collections.EMPTY_LIST;
         }
 
+        List<Integer> result = new ArrayList<>();
         int left = 0;
         int right = matrix[0].length - 1;
         int up = 0;
         int down = matrix.length - 1;
 
-        while (left < right && up < down) {
+        while (left <= right && up <= down) {
             for (int i = left; i <= right; i++) {
-                System.out.print(matrix[up][i] + " ");
+                result.add(matrix[up][i]);
             }
             up++;
 
             for (int i = up; i <= down; i++) {
-                System.out.print(matrix[i][right] + " ");
+                result.add(matrix[i][right]);
             }
             right--;
 
-            for (int i = right; i >= left; i--) {
-                System.out.print(matrix[down][i] + " ");
+            if (up <= down) {
+                for (int i = right; i >= left; i--) {
+                    result.add(matrix[down][i]);
+                }
             }
             down--;
 
-            for (int i = down; i >= up; i--) {
-                System.out.print(matrix[i][left] + " ");
+            if (left <= right) {
+                for (int i = down; i >= up; i--) {
+                    result.add(matrix[i][left]);
+                }
             }
             left++;
         }
+        return result;
     }
     
     public static void main(String args[]){
         SpiralPrinting sp = new SpiralPrinting();
-        int matrix[][] = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20}};
-        sp.print(matrix);
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}};
+        List<Integer> result = sp.spiralOrder(matrix);
+        System.out.print(result);
     }
 }
