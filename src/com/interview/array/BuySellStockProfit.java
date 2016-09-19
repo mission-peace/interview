c7c18b3f1a70ef163ea94e2057a7df5ccbe3329e
oklishch <oklishch@cybervisiontech.com>
2016-09-19 13:28:53 +0300

    Removed the redundant localMin variable.

--- src/com/interview/array/BuySellStockProfit.java
+++ src/com/interview/array/BuySellStockProfit.java
@@ -31,19 +31,11 @@ public class BuySellStockProfit {
     }
     
     public int allTimeProfit(int arr[]){
-        if (arr.length == 0) {
-            return 0;
-        }
         int profit = 0;
-        int localMin = arr[0];
         for(int i=1; i < arr.length;i++){
-            if(arr[i-1] >= arr[i]){
-                localMin = arr[i];
-            }else{
-                profit += arr[i] - localMin;
-                localMin = arr[i];
+            if(arr[i-1] < arr[i]){
+                profit += arr[i] - arr[i-1];
             }
-            
         }
         return profit;
     }
