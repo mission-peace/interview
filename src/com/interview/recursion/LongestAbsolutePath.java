1ed843bb34394fe98ee8125b65386e61634a9aaf
Tushar Roy <tushar_v_roy@Tushars-MacBook-Pro.local>
2016-10-19 22:28:18 -0700

    Add another method in longest absolute path

--- src/com/interview/recursion/LongestAbsolutePath.java
+++ src/com/interview/recursion/LongestAbsolutePath.java
@@ -2,15 +2,21 @@ package com.interview.recursion;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 /**
  * Find longest absolute path in file system indicated by \n and \t.
  *
- * Solution:
+ * Solution 1:
  * Create queue of file and its level. Then recurse from root towards file. Use level
  * to decide if next word in queue is in same level (so no more recursion) or next level so
  * going deep in recursion.
+ *
+ * Solution 2(iterative):
+ * Keep pushing elements into stack till you either reach a file with . or a file whose number of tabs is less than
+ * top of stack. If you reach file with extension then update the max. Otherwise keep popping from stack till number of tabs
+ * at top of stack becomes less than current file. Maintain current length during push and pop.
  *
  * Time complexity O(n)
  * Space complexity O(n)
@@ -73,5 +79,41 @@ public class LongestAbsolutePath {
             this.level = numberOfTabs;
             this.isFile = file.contains(".");
         }
+    }
+
+    public int lengthLongestPathIterative(String input) {
+        if (input.length() == 0) {
+            return 0;
+        }
+        String[] tokens = input.split("\n");
+        Stack<String> stack = new Stack<>();
+        Stack<Integer> stack1 = new Stack<>();
+        int len = 0;
+        int max = 0;
+        for (String token : tokens) {
+            int level = 0;
+            int i;
+            for (i = 0; i < token.length(); i++) {
+                if (token.charAt(i) == '\t') {
+                    level++;
+                } else {
+                    break;
+                }
+            }
+            token = token.substring(i);
+            while (!stack1.isEmpty() && level <= stack1.peek()) {
+                stack1.pop();
+                String data = stack.pop();
+                len -= data.length() + 1; //+1 to account for '\' between folders files
+            }
+            if (token.contains(".")) {
+                max = Math.max(max, len + token.length());
+            } else {
+                stack1.push(level);
+                stack.push(token);
+                len += token.length() + 1; //+1 to accoutn for '\' between folders files
+            }
+        }
+        return max;
     }
 }
