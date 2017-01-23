6e9f39347600d6d80f84e4fad7d98bc6ad7fea5c
Tushar Roy <tushar_v_roy@apple.com>
2017-01-22 18:33:37 -0800

    update search in sorted rotated array

--- src/com/interview/binarysearch/SortedAndRotatedArraySearch.java
+++ src/com/interview/binarysearch/SortedAndRotatedArraySearch.java
@@ -1,13 +1,24 @@
 package com.interview.binarysearch;
 
 /**
- * http://www.geeksforgeeks.org/search-an-element-in-a-sorted-and-pivoted-array/
- * This does not works if there are duplicates in the array.
+ * @author Tushar Roy
+ * Date 01/22/17
+ *
+ * Search in sorted and rotated array. In one version duplicate is not allowed and
+ * in another version duplicate is allowed.
+ *
+ * Time complexity with no duplicate - O(logn)
+ * Time complexity with duplicates - O(n)
+ *
+ * https://leetcode.com/problems/search-in-rotated-sorted-array/
+ * https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
  */
 public class SortedAndRotatedArraySearch {
 
+    /**
+     * Duplicates are not allowed in arr.
+     */
     public int search(int arr[],int search){
-        
         int low =0;
         int high = arr.length-1;
         while(low <= high){
@@ -31,6 +42,39 @@ public class SortedAndRotatedArraySearch {
             }
         }
         return -1;
+    }
+
+    /**
+     * Duplicates are allowed in arr.
+     */
+    public boolean searchWithDuplicates(int[] arr, int search) {
+        int low =0;
+        int high = arr.length-1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(arr[mid] == search) {
+                return true;
+            }
+            //if low is same as mid then increment low.
+            if (arr[mid] == arr[low]) {
+                low++;
+            } else if (arr[mid] == arr[high]) { //if high is same as mid then decrement high.
+                high--;
+            } else if (arr[mid] < arr[high]) {
+                if(arr[mid] < search && search <= arr[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            } else {
+                if(search >= arr[low] && search < arr[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return false;
     }
     
     public static void main(String args[]){
