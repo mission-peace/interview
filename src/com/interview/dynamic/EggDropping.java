f604ee15bde1feaa3fdfdd029ed82ad91409967a
Duy Dang <duydanglife@gmail.com>
2017-02-27 16:59:22 -0800

    more notes

--- src/com/interview/dynamic/EggDropping.java
+++ src/com/interview/dynamic/EggDropping.java
@@ -9,16 +9,20 @@ public class EggDropping {
         
         int T[][] = new int[eggs+1][floors+1];
         int c =0;
+        // need one trial per floor given one egg
         for(int i=0; i <= floors; i++){
             T[1][i] = i;
         }
         
+        // Fill rest of the entries in table using optimal substructure property
         for(int e = 2; e <= eggs; e++){
             for(int f = 1; f <=floors; f++){
                 T[e][f] = Integer.MAX_VALUE;
                 for(int k = 1; k <=f ; k++){
-                    c = 1 + Math.max(T[e-1][k-1], T[e][f-k]);
-                    if(c < T[e][f]){
+                    c = 1 + // count current attempt plus worst case subproblem
+                        Math.max(T[e-1][k-1], // egg breaks, obtain results for lower floors
+                        T[e][f-k]); // egg survives, obtain subproblem result
+                    if(c < T[e][f]){ // keep best(min) value
                         T[e][f] = c;
                     }
                 }
