a862ad51edbfd8f78a323ce936b14f07dccd6757
Duy Dang <duydanglife@gmail.com>
2017-03-02 14:44:57 -0800

    more notes

--- src/com/interview/dynamic/TextJustification.java
+++ src/com/interview/dynamic/TextJustification.java
@@ -33,26 +33,35 @@ package com.interview.dynamic;
  */
 public class TextJustification {
 
+    // time complexity: O(n^2)
+    // space complexity: O(n^2)
     public String justify(String words[], int width) {
-        
+        // cost refers to empty-space^2
         int cost[][] = new int[words.length][words.length];
         
         //next 2 for loop is used to calculate cost of putting words from
         //i to j in one line. If words don't fit in one line then we put
         //Integer.MAX_VALUE there.
+        // O(n^2) for double loop
         for(int i=0 ; i < words.length; i++){
+            //initialize \ diagonal with unused value
             cost[i][i] = width - words[i].length();
             for(int j=i+1; j < words.length; j++){
+                // initialize matrix above \ diagonal (top-right)
+                // subtract next word from remaining width, -1 for space
+                // this yields negative values for lines that don't fit
                 cost[i][j] = cost[i][j-1] - words[j].length() - 1; 
             }
         }
         
         for(int i=0; i < words.length; i++){
             for(int j=i; j < words.length; j++){
-                if(cost[i][j] < 0){
+                if(cost[i][j] < 0){ 
+                    // lines that don't fit are set to maxInt
                     cost[i][j] = Integer.MAX_VALUE;
                 }else{
-                    cost[i][j] = (int)Math.pow(cost[i][j], 2);
+                    // cost is sum-of-square of char unused in width
+                    cost[i][j] = (int)Math.pow(cost[i][j], 2); // square unused value
                 }
             }
         }
@@ -60,18 +69,24 @@ public class TextJustification {
         //minCost from i to len is found by trying
         //j between i to len and checking which
         //one has min value
+        // stores minimumCost of index to result[sameIndex-1]
         int minCost[] = new int[words.length];
+        // words[value] is 1st word of new line
         int result[] = new int[words.length];
-        for(int i = words.length-1; i >= 0 ; i--){
-            minCost[i] = cost[i][words.length-1];
-            result[i] = words.length;
-            for(int j=words.length-1; j > i; j--){
-                if(cost[i][j-1] == Integer.MAX_VALUE){
+        // S: start word, E-1: end word
+        for(int S = words.length-1; S >= 0 ; S--){
+            minCost[S] = cost[S][words.length-1];
+            result[S] = words.length;
+            for(int E=words.length-1; E > S; E--){
+                if(cost[S][E-1] == Integer.MAX_VALUE){ // skip invalid line
                     continue;
                 }
-                if(minCost[i] > minCost[j] + cost[i][j-1]){
-                    minCost[i] = minCost[j] + cost[i][j-1];
-                    result[i] = j;
+                //cost[S][E-1] is cost of current line
+                //minCost[E] is cost of next line to end
+                //minCost[0] is cost of all lines
+                if(minCost[S] > minCost[E] + cost[S][E-1]){
+                    minCost[S] = minCost[E] + cost[S][E-1];
+                    result[S] = E;
                 }
             }
         }
