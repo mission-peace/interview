1ea5def514c15411bb5ffe5d8dff0d8223b77109
Duy Dang <duydanglife@gmail.com>
2017-03-10 12:39:07 -0800

    logic for NON-search binary tree

--- src/com/interview/tree/LowestCommonAncestorInBinaryTree.java
+++ src/com/interview/tree/LowestCommonAncestorInBinaryTree.java
@@ -12,19 +12,24 @@ package com.interview.tree;
 public class LowestCommonAncestorInBinaryTree {
 
     public Node lca(Node root, Node n1, Node n2){
-        if(root == null){
+        if(root == null){ // base case
             return null;
         }
+        // return valid node
         if(root == n1 || root == n2){
-            return root;
+            return root; 
         }
         
+        // recursive traversal of tree
         Node left = lca(root.left, n1, n2);
         Node right = lca(root.right, n1, n2);
-
+        
+        // root is LCA since 
+        // target node is found on left && right branch
         if(left != null && right != null){
             return root;
         }
+        // return node that is not null; therefore, valid
         return left != null ? left : right;
     }
 }
