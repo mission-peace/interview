183de817b302124e63d915469d9b9829230bc32d
Duy Dang <duydanglife@gmail.com>
2017-03-09 18:45:45 -0800

    logic notes

--- src/com/interview/tree/TreeTraversalInSpiralOrder.java
+++ src/com/interview/tree/TreeTraversalInSpiralOrder.java
@@ -47,11 +47,12 @@ public class TreeTraversalInSpiralOrder {
         Stack<Node> s1 = new Stack<>();
         Stack<Node> s2 = new Stack<>();
         s1.push(root);
-
+        // stack1's children are added to stack2
         while (!s1.isEmpty() || !s2.isEmpty()) {
             while (!s1.isEmpty()) {
                 root = s1.pop();
                 System.out.print(root.data + " ");
+                // stack2 is built leftNode to rightNode
                 if (root.left != null) {
                     s2.push(root.left);
                 }
@@ -59,9 +60,11 @@ public class TreeTraversalInSpiralOrder {
                     s2.push(root.right);
                 }
             }
+            // stack2's children are added to stack1
             while (!s2.isEmpty()) {
                 root = s2.pop();
                 System.out.print(root.data + " ");
+                // stack1 is built rightNode to leftNode
                 if (root.right != null) {
                     s1.push(root.right);
                 }
@@ -74,6 +77,8 @@ public class TreeTraversalInSpiralOrder {
 
     /**
      * One deque with count method to print tree in spiral order
+     * flips between pollFirst, offerLast leftChildNode to rightChildNode &&
+     * pollLast, offerFirst rightChildNode to leftChildNode
      */
     public void spiralWithOneDeque(Node root) {
         if (root == null) {
@@ -81,38 +86,39 @@ public class TreeTraversalInSpiralOrder {
         }
         Deque<Node> deque = new LinkedList<Node>();
         deque.offerFirst(root);
-        int count = 1;
+        int remainingNodes = 1;
         boolean flip = true;
         while (!deque.isEmpty()) {
-            int currentCount = 0;
-            while (count > 0) {
+            int childrenremainingNodes = 0;
+            while (remainingNodes > 0) {
                 if (flip) {
                     root = deque.pollFirst();
                     System.out.print(root.data + " ");
                     if (root.left != null) {
                         deque.offerLast(root.left);
-                        currentCount++;
+                        childrenremainingNodes++;
                     }
                     if (root.right != null) {
                         deque.offerLast(root.right);
-                        currentCount++;
+                        childrenremainingNodes++;
                     }
                 } else {
                     root = deque.pollLast();
                     System.out.print(root.data + " ");
                     if (root.right != null) {
                         deque.offerFirst(root.right);
-                        currentCount++;
+                        childrenremainingNodes++;
                     }
                     if (root.left != null) {
                         deque.offerFirst(root.left);
-                        currentCount++;
+                        childrenremainingNodes++;
                     }
                 }
-                count--;
-            }
-            flip = !flip;
-            count = currentCount;
+                remainingNodes--;
+            } // remainingNodes == 0
+            flip = !flip; // flip direction
+            // childrenNodes added this cycle becomes nodes to print
+            remainingNodes = childrenremainingNodes; // allows reentry of loop
         }
     }
 
@@ -129,28 +135,34 @@ public class TreeTraversalInSpiralOrder {
         q.offerFirst(root);
         //if only delimiter(in this case null) is left in queue then break
         while(q.size() > 1){
+            // in front of null, queue is built leftNode to rightNode
             root = q.peekFirst();
             while(root != null){
                 root = q.pollFirst();
                 System.out.print(root.data + " ");
+                // build queue behindNull with beforeNull's children
                 if(root.left != null){
                     q.offerLast(root.left);
                 }
                 if(root.right != null){
                     q.offerLast(root.right);
                 }
+                // continue front loop until null
                 root = q.peekFirst();
-            }
+            } // front node is null, enter back loop
+            // behind null's queue is build rightNode to leftNode
             root = q.peekLast();
             while(root != null){
                 System.out.print(root.data + " ");
                 root = q.pollLast();
+                // build queue beforeNull with behindNull's children
                 if(root.right != null){
                     q.offerFirst(root.right);
                 }
                 if(root.left != null){
                     q.offerFirst(root.left);
                 }
+                // continue back loop until null
                 root = q.peekLast();
             }
         }
