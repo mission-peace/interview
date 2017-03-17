382ebdd17ce184383158ae25e711af7042ea87fe
Duy Dang <duydanglife@gmail.com>
2017-03-17 14:11:34 -0700

    renamed loop variables

--- src/com/interview/graph/FloydWarshallAllPairShortestPath.java
+++ src/com/interview/graph/FloydWarshallAllPairShortestPath.java
@@ -11,8 +11,8 @@ import java.util.Map;
  *
  * Floyd-Warshall Algorithm for finding all pair shortest path.
  *
- * Time complexity - O(V^3)
- * Space complexity - O(V^2)
+ * Time complexity - O(V^3), row vertex * column vertex * through vertex
+ * Space complexity - O(V^2), vertex matrix size
  *
  * References
  * https://en.wikipedia.org/wiki/Floyd%E2%80%93Warshall_algorithm
@@ -23,33 +23,44 @@ public class FloydWarshallAllPairShortestPath {
 
     }
 
+    // big number treated as infinity. 
+    // INTEGER_MAX may cause integer overflow with addition
     private static final int INF = 1000000;
 
     public int[][] allPairShortestPath(int[][] distanceMatrix) {
         
+        // distance from [row] to [column]
+        // infinity means there is no path from [row] to [column]
         int distance[][] = new int[distanceMatrix.length][distanceMatrix.length];
+        // vertex 'v' traversed to find shortest path 
+        // corresponding to distance matrix
         int path[][] = new int[distanceMatrix.length][distanceMatrix.length];
 
-        for (int i=0; i < distanceMatrix.length; i++) {
-            for (int j=0; j< distanceMatrix[i].length; j++){
-                distance[i][j] = distanceMatrix[i][j];
-                if (distanceMatrix[i][j] != INF && i != j) {
-                    path[i][j] = i;
+        for (int r=0; r < distanceMatrix.length; r++) { //r: row
+            for (int c=0; c< distanceMatrix[r].length; c++){ //c: column
+                distance[r][c] = distanceMatrix[r][c];
+                if (distanceMatrix[r][c] != INF && r != c) {
+                    // initial path from [row] to [column] is row
+                    path[r][c] = r;
                 } else {
-                    path[i][j] = -1;
+                    // path does not exist
+                    path[r][c] = -1;
                 }
             }
         }
 
-        for(int k=0; k < distanceMatrix.length; k++){
-            for(int i=0; i < distanceMatrix.length; i++){
-                for(int j=0; j < distanceMatrix.length; j++){
-                    if(distance[i][k] == INF || distance[k][j] == INF) {
+        // check if going through vertex 'v' will give shorter distance
+        for(int v=0; v < distanceMatrix.length; v++){ // v: vertex
+            for(int r=0; r < distanceMatrix.length; r++){ //r: row
+                for(int c=0; c < distanceMatrix.length; c++){ //c: column
+                    if(distance[r][v] == INF || distance[v][c] == INF) {
+                        // no path from rowVertex->vertex || vertex->columnVertex
                         continue;
                     }
-                    if(distance[i][j] > distance[i][k] + distance[k][j]){
-                        distance[i][j] = distance[i][k] + distance[k][j];
-                        path[i][j] = path[k][j];
+                    if(distance[r][c] > distance[r][v] + distance[v][c]){
+                        distance[r][c] = distance[r][v] + distance[v][c];
+                        // copy vertex used to find shorter path
+                        path[r][c] = path[v][c];
                     }
                 }
             }
@@ -58,18 +69,21 @@ public class FloydWarshallAllPairShortestPath {
         //look for negative weight cycle in the graph
         //if values on diagonal of distance matrix is negative
         //then there is negative weight cycle in the graph.
+        // distance from vertex to itself should be 0
         for(int i = 0; i < distance.length; i++) {
             if(distance[i][i] < 0) {
                 throw new NegativeWeightCycleException();
             }
         }
 
-        printPath(path, 3, 2);
+//        printPath(path, 3, 2);
         return distance;
     }
 
     public void printPath(int[][] path, int start, int end) {
-        if(start < 0 || end < 0 || start >= path.length || end >= path.length) {
+        if(start < 0 || end < 0 
+                || start >= path.length 
+                || end >= path.length) {
             throw new IllegalArgumentException();
         }
 
@@ -102,7 +116,8 @@ public class FloydWarshallAllPairShortestPath {
                 {1,   INF, INF, 0}
         };
 
-        FloydWarshallAllPairShortestPath shortestPath = new FloydWarshallAllPairShortestPath();
+        FloydWarshallAllPairShortestPath shortestPath = 
+                new FloydWarshallAllPairShortestPath();
         int[][] distance = shortestPath.allPairShortestPath(graph);
         System.out.println("Minimum Distance matrix");
         for(int i=0; i < distance.length; i++){
