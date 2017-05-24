1def8b96222f33baaf80aae63edca523b7a5db08
rubenMountainView <bhatt.ruben@gmail.com>
2017-05-24 16:09:49 +0530

    Update MatrixMultiplicationCost.java
    l=2 will lead to accesing values not computed yet so l=1 will be correct

--- src/com/interview/dynamic/MatrixMultiplicationCost.java
+++ src/com/interview/dynamic/MatrixMultiplicationCost.java
@@ -8,7 +8,7 @@ public class MatrixMultiplicationCost {
     public int findCost(int arr[]){
         int temp[][] = new int[arr.length][arr.length];
         int q = 0;
-        for(int l=2; l < arr.length; l++){
+        for(int l=1; l < arr.length; l++){
             for(int i=0; i < arr.length - l; i++){
                 int j = i + l;
                 temp[i][j] = 1000000;
