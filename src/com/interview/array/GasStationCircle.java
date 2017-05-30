6cb78be2feac9edae549d9de04040739e9006e66
Sanchay Javeria <javeria2@illinois.edu>
2017-05-29 22:39:52 -0500

    simpler solution

--- src/com/interview/array/GasStationCircle.java
+++ src/com/interview/array/GasStationCircle.java
@@ -35,6 +35,45 @@ public class GasStationCircle {
         
         return end;
     }
+
+    /**
+     * If it is not guaranteed that tour exists then once you get
+     * result of kadanewrap make an actual trip to see if value is positive
+     * @return -1 if no solution exists otherwise returns gas station at which to start.
+     */
+    public int startTour1(int gasAvailable[], int gasRequired[]){
+        int diff[] = new int[gasAvailable.length];
+        for(int i=0; i < diff.length; i++){
+            diff[i] = gasAvailable[i] - gasRequired[i];
+        }
+
+        boolean allNegative = true;
+        for (int i = 0; i < diff.length; i++) {
+            if (diff[i] >= 0) {
+                allNegative = false;
+                break;
+            }
+        }
+
+        if (allNegative) {
+            return -1;
+        }
+
+        KadaneWrapArray kwa = new KadaneWrapArray();
+        Triplet t = kwa.kadaneWrap(diff);
+        //make sure this solution leads to answer
+        int i = t.start;
+        int netGas = 0;
+        do {
+            netGas += diff[i];
+            i = (i + 1)%diff.length;
+            if (netGas < 0) {
+                return -1;
+            }
+        } while (i != t.start);
+
+        return t.start;
+    }
     
     /**
      * A much simpler solution - 
@@ -45,7 +84,7 @@ public class GasStationCircle {
      * @param  gasRequired [required amount of gas to go from one station to next]
      * @return  start [index of starting point]
      */
-    public int startTour1(int gasAvailable[], int gasRequired[]){
+    public int startTour2(int gasAvailable[], int gasRequired[]){
         int totalDistance = 0;
         int totalGas = 0;
         int start = 0;
@@ -68,5 +107,6 @@ public class GasStationCircle {
         int[] gasRequired = {5, 6, 1};
         System.out.println(gsc.startTour(gasAvailable, gasRequired));
         System.out.println(gsc.startTour1(gasAvailable, gasRequired));
+        System.out.println(gsc.startTour2(gasAvailable, gasRequired));
     }
 }
