52445a40b1bca79991f9e2f1a0eba240fdecd44c
shaiktaj <shaiktaj2003@gmail.com>
2018-03-21 00:12:24 -0500

    Remove max variable and check
    Why do we need max variable? When we are checking till end of the strings isnt max value propagated till end?

--- src/com/interview/dynamic/LongestCommonSubsequence.java
+++ src/com/interview/dynamic/LongestCommonSubsequence.java
@@ -21,7 +21,6 @@ public class LongestCommonSubsequence {
     public int lcsDynamic(char str1[],char str2[]){
     
         int temp[][] = new int[str1.length + 1][str2.length + 1];
-        int max = 0;
         for(int i=1; i < temp.length; i++){
             for(int j=1; j < temp[i].length; j++){
                 if(str1[i-1] == str2[j-1]) {
@@ -30,13 +29,10 @@ public class LongestCommonSubsequence {
                 else
                 {
                     temp[i][j] = Math.max(temp[i][j-1],temp[i-1][j]);
-                }
-                if(temp[i][j] > max){
-                    max = temp[i][j];
-                }
+                }                
             }
         }
-        return max;
+        return temp[str1.length][str2.length];
     
     }
     
