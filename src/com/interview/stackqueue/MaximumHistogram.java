85faecc269ba33172605635c425a85ab27abd44c
ssarthak-s <ssarthaksota@gmail.com>
2018-06-14 01:01:56 +0530

    Simplified the code by intserting -1 at the end of the array so that we donot have to keep the track of i.

--- src/com/interview/stackqueue/MaximumHistogram.java
+++ src/com/interview/stackqueue/MaximumHistogram.java
@@ -38,56 +38,44 @@ import java.util.LinkedList;
 public class MaximumHistogram {
 
     public int maxHistogram(int input[]){
-        Deque<Integer> stack = new LinkedList<Integer>();
+        Stack<Integer> s=new Stack<Integer>();
         int maxArea = 0;
         int area = 0;
         int i;
-        for(i=0; i < input.length;){
-            if(stack.isEmpty() || input[stack.peekFirst()] <= input[i]){
-                stack.offerFirst(i++);
-            }else{
-                int top = stack.pollFirst();
+        for(i=0; i < input.length;i++){
+           if(s.size()==0){
+                s.push(i);
+                        }
+            else{
+                    //if height at current index in greater than height at index=s.peek()
+                    //then while loop will not execute and current index will directly be
+                    //pushed to the stack
+                    while(s.size()!=0 && input[i]<=input[s.peek()]){
+                        area=0;
+                       
+                       int height=input[s.pop()];
                 //if stack is empty means everything till i has to be
-                //greater or equal to input[top] so get area by
-                //input[top] * i;
-                if(stack.isEmpty()){
-                    area = input[top] * i;
-                }
+                //greater or equal to input[top] so width =i ;
+                
                 //if stack is not empty then everythin from i-1 to input.peek() + 1
                 //has to be greater or equal to input[top]
-                //so area = input[top]*(i - stack.peek() - 1);
-                else{
-                    area = input[top] * (i - stack.peekFirst() - 1);
-                }
-                if(area > maxArea){
-                    maxArea = area;
-                }
+                //so width = i - stack.peek() - 1;
+                       int width=s.size()==0?i:i-s.peek()-1;
+                       area=m1*m2;
+                       if(area>maxArea){maxArea=area;}
+                   }
+                
+                s.push(i); 
+               
             }
         }
-        while(!stack.isEmpty()){
-            int top = stack.pollFirst();
-            //if stack is empty means everything till i has to be
-            //greater or equal to input[top] so get area by
-            //input[top] * i;
-            if(stack.isEmpty()){
-                area = input[top] * i;
-            }
-            //if stack is not empty then everything from i-1 to input.peek() + 1
-            //has to be greater or equal to input[top]
-            //so area = input[top]*(i - stack.peek() - 1);
-            else{
-                area = input[top] * (i - stack.peekFirst() - 1);
-            }
-        if(area > maxArea){
-                maxArea = area;
-            }
-        }
+    
         return maxArea;
     }
     
     public static void main(String args[]){
         MaximumHistogram mh = new MaximumHistogram();
-        int input[] = {2,2,2,6,1,5,4,2,2,2,2};
+        int input[] = {2,2,2,6,1,5,4,2,2,2,2,-1};
         int maxArea = mh.maxHistogram(input);
         //System.out.println(maxArea);
         assert maxArea == 12;
