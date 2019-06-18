1f13dd5b02ba4cc29c4963d56246335279025186
Sandeep1991 <sandeepvadiraj@yahoo.co.in>
2019-06-18 11:15:04 -0700

    Update RegexMatching.java

--- src/com/interview/dynamic/RegexMatching.java
+++ src/com/interview/dynamic/RegexMatching.java
@@ -52,14 +52,14 @@ public class RegexMatching {
 
         T[0][0] = true;
         //Deals with patterns like a* or a*b* or a*b*c*
-        for (int i = 1; i < T[0].length; i++) {
+        for (int i = 1; i <= T[0].length; i++) {
             if (pattern[i-1] == '*') {
                 T[0][i] = T[0][i - 2];
             }
         }
 
-        for (int i = 1; i < T.length; i++) {
-            for (int j = 1; j < T[0].length; j++) {
+        for (int i = 1; i <= T.length; i++) {
+            for (int j = 1; j <= T[0].length; j++) {
                 if (pattern[j - 1] == '.' || pattern[j - 1] == text[i - 1]) {
                     T[i][j] = T[i-1][j-1];
                 } else if (pattern[j - 1] == '*')  {
