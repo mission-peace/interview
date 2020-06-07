a292655c6a81eaf836a28ab4bb5cb4296d32b6e4
Kushal V. Mahajan <11805936+kushalmahajan@users.noreply.github.com>
2020-06-07 06:51:13 +0530

    added check for j > 1 while filling first row
    1. added check for j > 1 while filling first row
    2. Updated i to j to maintain convention of filling column and to have consistent representation in our code as well.
    3. added comment for // pattern[0] cannot be '*' so no need to check "j > 1" here inside the two loops to have greater clarity.

--- src/com/interview/dynamic/RegexMatching.java
+++ src/com/interview/dynamic/RegexMatching.java
@@ -52,9 +52,9 @@ public class RegexMatching {
 
         T[0][0] = true;
         //Deals with patterns like a* or a*b* or a*b*c*
-        for (int i = 1; i < T[0].length; i++) {
-            if (pattern[i-1] == '*') {
-                T[0][i] = T[0][i - 2];
+        for (int j = 1; j < T[0].length; j++) {
+            if (j > 1 && pattern[j-1] == '*') {
+                T[0][j] = T[0][j - 2];
             }
         }
 
@@ -63,12 +63,11 @@ public class RegexMatching {
                 if (pattern[j - 1] == '.' || pattern[j - 1] == text[i - 1]) {
                     T[i][j] = T[i-1][j-1];
                 } else if (pattern[j - 1] == '*')  {
+                    // pattern[0] cannot be '*' so no need to check "j > 1" here
                     T[i][j] = T[i][j - 2];
                     if (pattern[j-2] == '.' || pattern[j - 2] == text[i - 1]) {
                         T[i][j] = T[i][j] | T[i - 1][j];
                     }
-                } else {
-                    T[i][j] = false;
                 }
             }
         }
