1b4d749e547d3d41d8e419649f7cc9ee35bae66b
Shivansh Mehrotra <43709695+Shivanshdev12@users.noreply.github.com>
2020-07-28 21:06:58 +0530

    Added isBST

---
+++ C++/Trees/isBST.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include<queue>
+#include<climits>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node*left;
+        node*right;
+
+        node(int d){
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+//Accepts the old root node & data and returns the new root node 
+node* insertInBST(node *root,int data){
+    //Base Case
+    if(root==NULL){
+        return new node(data);
+    }
+    //Rec Case - Insert in the Subtree and Update Pointers
+    if(data<=root->data){
+        root->left = insertInBST(root->left,data);
+    }
+    else{
+        root->right = insertInBST(root->right,data);
+    }
+    return root;
+}
+
+
+//Build tree
+node* build(){
+    //Read a list of numbers till -1 and also these numbers will be inserted into BST
+    int d;
+    cin>>d;
+
+    node*root = NULL;
+
+    while(d!=-1){
+        root = insertInBST(root,d);
+        cin>>d;
+    }
+    return root;
+}
+
+
+//Inorder Print
+void inorder(node*root){
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<",";
+    inorder(root->right);
+}
+
+//Is Binary search tree
+bool isBST(node *root,int minV = INT_MIN,int maxV = INT_MAX){
+    if(root==NULL){
+        return true;
+    }
+    if(root->data >= minV && root->data<=maxV && isBST(root->left,minV,root->data) && isBST(root->right,root->data,maxV)){
+        return true;
+    }
+    return false;
+}
+
+
+int main(){ 
+    node*root = build();
+    inorder(root);
+    cout<<endl;
+
+    if(isBST(root)){
+        cout<<"Yes";
+    }
+    else{
+        cout<<"Not a BST!";
+    }
+   return 0;
+}
