3b010e34dd7702705d81d598c83d60569a346fff
Yussif Mohammed <yousiph77@gmail.com>
2022-06-16 10:30:32 +0000

    Removed else block
    Since there is a return statement in the ```if``` block ```else``` not necessary

--- src/com/interview/dynamic/CountNumberOfTreesInBST.java
+++ src/com/interview/dynamic/CountNumberOfTreesInBST.java
@@ -10,16 +10,15 @@ public class CountNumberOfTreesInBST {
         if (numKeys <=1) {
             return(1);
         }
-        else {
-            int sum = 0;
-            int left, right, root;
-            for (root=1; root<=numKeys; root++) {
-                left = countTreesRec(root - 1);
-                right = countTreesRec(numKeys - root);
-                sum += left*right;
-            }
-            return(sum);
+     
+        int sum = 0;
+        int left, right, root;
+        for (root=1; root<=numKeys; root++) {
+            left = countTreesRec(root - 1);
+            right = countTreesRec(numKeys - root);
+            sum += left*right;
         }
+        return(sum);
     }
     
     public int countTrees(int n){
